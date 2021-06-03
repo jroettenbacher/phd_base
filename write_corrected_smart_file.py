f74ba59123d3de994a9a05f4746c25340455d145
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-03 17:04:15 +0200

    added dark correction run for calib files

--- write_corrected_smart_file.py
+++ write_corrected_smart_file.py
@@ -8,10 +8,17 @@ import os
 import smart
 
 # Set paths in config.toml
-raw_path, _, _, data_path = smart.set_paths()
+raw_path, _, calib_path, data_path, _ = smart.set_paths()
 files = os.listdir(raw_path)
+
+# # correct calibration files for dark current
+# for dirpath, dirs, files in os.walk(calib_path):
+#     for file in files:
+#         if file.endswith("IR.dat"):
+#             print(file)
+#             smart_cor = smart.correct_smart_dark_current(file, option=2, path=dirpath)
+#             smart_cor.to_csv(f"{dirpath}/{file.replace('.dat', '_cor.dat')}", sep="\t", float_format="%.0f")
 
 for file in files:
     smart_cor = smart.correct_smart_dark_current(file, option=2)
     smart_cor.to_csv(f"{data_path}/{file.replace('.dat', '_cor.dat')}", sep="\t", float_format="%.0f")
-
