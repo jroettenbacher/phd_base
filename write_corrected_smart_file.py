92f6c3e654769bb18d0f64dc51a0af206027988e
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-08 14:18:14 +0200

    added folder variable

--- write_corrected_smart_file.py
+++ write_corrected_smart_file.py
@@ -11,13 +11,14 @@ import smart
 raw_path, _, calib_path, data_path, _ = smart.set_paths()
 files = os.listdir(raw_path)
 
-# # correct calibration files for dark current
-# for dirpath, dirs, files in os.walk(calib_path):
-#     for file in files:
-#         if file.endswith("IR.dat"):
-#             print(file)
-#             smart_cor = smart.correct_smart_dark_current(file, option=2, path=dirpath)
-#             smart_cor.to_csv(f"{dirpath}/{file.replace('.dat', '_cor.dat')}", sep="\t", float_format="%.0f")
+folder = "ASP_07_transfer_calib_20210607"  # name specific folder if you only want to run this one, else set this to ""
+# correct calibration files for dark current
+for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
+    for file in files:
+        if file.endswith("IR.dat"):
+            print(file)
+            smart_cor = smart.correct_smart_dark_current(file, option=2, path=dirpath)
+            smart_cor.to_csv(f"{dirpath}/{file.replace('.dat', '_cor.dat')}", sep="\t", float_format="%.0f")
 
 for file in files:
     smart_cor = smart.correct_smart_dark_current(file, option=2)
