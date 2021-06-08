dd8b9fd638bfde64abbda83fbdeb1276088ada59
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-08 14:15:27 +0200

    added folder variable to specifiy which folder and subfolder to merge in, added loop over channels

--- merge_minutely_files.py
+++ merge_minutely_files.py
@@ -14,17 +14,19 @@ import pandas as pd
 
 # combine minutely files from ASP_07 to one file
 _, _, calib_path, _, _ = set_paths()
-for dirpath, dirs, files in os.walk(os.path.join(calib_path, "ASP_07_Calib_Lab_20210318")):
-    try:
-        channel = "SWIR"  # replace SWIR with VNIR and run again
-        filename = [file for file in files if file.endswith(f"{channel}.dat")]
-        df = pd.concat([pd.read_csv(f"{dirpath}/{file}", sep="\t", header=None) for file in files
-                        if file.endswith(f"{channel}.dat")])
-        # delete all minutely files
-        for file in filename:
-            os.remove(os.path.join(dirpath, file))
-        outname = f"{dirpath}/{filename[0]}"
-        df.to_csv(outname, sep="\t", index=False, header=False)
-        print(f"Saved {outname}")
-    except ValueError:
-        pass
+folder = "ASP_07_transfer_calib_20210607"
+channels = ["SWIR", "VNIR"]
+for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
+    for channel in channels:
+        try:
+            # channel = "VNIR"  # replace SWIR with VNIR and run again
+            filename = [file for file in files if file.endswith(f"{channel}.dat")]
+            df = pd.concat([pd.read_csv(f"{dirpath}/{file}", sep="\t", header=None) for file in filename])
+            # delete all minutely files
+            for file in filename:
+                os.remove(os.path.join(dirpath, file))
+            outname = f"{dirpath}/{filename[0]}"
+            df.to_csv(outname, sep="\t", index=False, header=False)
+            print(f"Saved {outname}")
+        except ValueError:
+            pass
