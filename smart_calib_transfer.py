8365314a34b19862db4fa61e8d23970db2fa8bfc
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-10 10:23:33 +0200

    script to calculate field calibration factor and write to file

---
+++ smart_calib_transfer.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""Script to calculate field calibration factor and save a file for each PC and channel
+author: Johannes Roettenbacher
+"""
+# %% module import and set paths
+import smart
+from smart import lookup
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+raw_path, pixel_path, calib_path, data_path, plot_path = smart.set_paths()
+
+# %% set variables
+# field_folder = "ASP_06_transfer_calib_20210604"  # transfer calib folder
+field_folder = "ASP_07_transfer_calib_20210607"  # transfer calib folder
+t_int = 200  # integration time of calibration measurement
+# list transfer calibration files
+field_cali_files = [f for f in os.listdir(f"{calib_path}/{field_folder}/Tint_{t_int}ms") if f.endswith("cor.dat")]
+# list lab calibration files for selecting the right one
+lab_cali_files = [f for f in os.listdir(calib_path) if f.endswith("lab_calib.dat")]
+
+# %% read in Ulli transfer measurement from field
+for field_file in field_cali_files:
+    # field_file = "2021_06_04_13_40.Fdw_VNIR_cor.dat"
+    date_str, channel, direction = smart.get_info_from_filename(field_file)
+    ulli_field = smart.read_smart_cor(f"{calib_path}/{field_folder}/Tint_{t_int}ms", field_file)
+    ulli_field[ulli_field.values < 0] = 0  # set negative counts to 0
+    # read corresponding cali file
+    lab_file = [f for f in lab_cali_files if f"{direction}_{channel}" in f][0]
+    lab_df = pd.read_csv(f"{calib_path}/{lab_file}")
+    lab_df["S_ulli_field"] = ulli_field.mean().reset_index(drop=True)  # take mean over time of field calib measurement
+    lab_df["c_field"] = lab_df["F_ulli"] / lab_df["S_ulli_field"]  # calculate field calibration factor
+    # calculate relation between S_ulli_lab and S_ulli_field
+    lab_df["rel_ulli"] = lab_df["S_ulli"] / lab_df["S_ulli_field"]
+
+    # %% plot transfer calib measurement
+    spectrometer = lookup[f'{direction}_{channel}']
+    fig, ax = plt.subplots()
+    ax.plot(lab_df["wavelength"], lab_df["S_ulli"], label="Counts in lab")
+    ax.plot(lab_df["wavelength"], lab_df["S_ulli_field"], label="Counts in field")
+    ax.set_title(f"Ulli Transfer Sphere Lab and Transfer Calibration {date_str.replace('_', '-')} \n"
+                 f"{spectrometer} {direction} {channel}")
+    ax.set_xlabel("Wavelength (nm)")
+    ax.set_ylabel("Counts")
+    ax2 = ax.twinx()
+    ax2.plot(lab_df["wavelength"], lab_df["rel_ulli"], label="Lab / Field", color="green")
+    ax2.set_ylabel("$S_{ulli, lab} / S_{ulli, field}$")
+    # ask matplotlib for the plotted objects and their labels
+    lines, labels = ax.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines + lines2, labels + labels2, loc=0)
+    ax.grid()
+    plt.tight_layout()
+    figname = f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_ulli_transfer_calib.png"
+    plt.savefig(figname, dpi=100)
+    plt.show()
+    plt.close()
+    print(f"Saved {figname}")
+
+    # %% write output file
+    outfile = f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_transfer_calib.dat"
+    lab_df.to_csv(outfile, index=False)
+    print(f"Saved {outfile}")
