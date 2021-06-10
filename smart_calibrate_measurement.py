55757e3141ec867dacb0a1183e7868ed86a06b9b
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-10 15:38:01 +0200

    finished calibration process, read in files in loop and calibrate, save wide formatted data

--- smart_calibrate_measurement.py
+++ smart_calibrate_measurement.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python
 """Calibrate measurement files with the transfer calibration
-author: Johannes Roettenbacher"""
+author: Johannes Roettenbacher
+"""
 
-# read in dark current corrected measurement files
-measurement = smart.read_smart_cor(f"{data_path}/flight_00", "2021_03_29_11_15.Fdw_VNIR_cor.dat")
-# set negative values to 0
-measurement[measurement.values < 0] = 0
-# convert to long format
-m_long = measurement.melt(var_name="pixel", value_name="counts", ignore_index=False)
-df = m_long.merge(lab_df.loc[:, ["pixel", "c_field"]], on="pixel", right_index=True)
-df["F_x"] = df["counts"] * df["c_field"]
-df.pivot(columns="pixel", values="F_x")
+# %% import modules and set paths
+import os
+import pandas as pd
+import smart
+from smart import lookup
+from functions_jr import make_dir
+
+flight = "flight_00"  # set flight folder
+_, _, calib_path, data_path, _ = smart.set_paths()
+calibrated_path = smart.get_path("calibrated")
+inpath = f"{data_path}/{flight}"
+outpath = f"{calibrated_path}/{flight}"
+make_dir(outpath)  # create outpath if necessary
+
+# %% read in dark current corrected measurement files
+files = [f for f in os.listdir(inpath)]
+for file in files:
+    date_str, channel, direction = smart.get_info_from_filename(file)
+    spectrometer = lookup[f"{direction}_{channel}"]
+    measurement = smart.read_smart_cor(f"{data_path}/{flight}", file)
+    measurement[measurement.values < 0] = 0  # set negative values to 0
+
+    # %% read in matching transfer calibration file from same day
+    cali_file = f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_transfer_calib.dat"
+    print(f"Calibration file used:\n {cali_file}")
+    cali = pd.read_csv(cali_file)
+    # convert to long format
+    m_long = measurement.melt(var_name="pixel", value_name="counts", ignore_index=False)
+    # merge field calibration factor to long df on pixel column
+    df = m_long.join(cali.set_index(cali.pixel)["c_field"], on="pixel")
+    df[direction] = df["counts"] * df["c_field"]  # calculate calibrated radiance/irradiance
+
+    # %% save wide format calibrated measurement
+    df_out = df.pivot(columns="pixel", values=direction)  # convert to wide format (row=time, column=pixel)
+    outfile = f"{outpath}/{file.replace('.dat', '_calibrated.dat')}"
+    df_out.to_csv(outfile)
+    print(f"Saved {outfile}")
