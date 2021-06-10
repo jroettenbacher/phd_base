404eecf69ef7838945a74f7d0d1f340479063de4
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-10 10:01:27 +0200

    added print output and minor changes

--- smart_calib_lab_ASP06.py
+++ smart_calib_lab_ASP06.py
@@ -21,9 +21,9 @@ from scipy.interpolate import interp1d
 raw_path, pixel_path, calib_path, data_path, plot_path = smart.set_paths()
 
 # %% read lamp file
-lamp = smart.read_lamp_file()
+lamp = smart.read_lamp_file(plot=False, save_file=False, save_fig=False)
 
-# %% read in ASP06/ASP07 dark current corrected lamp measurement data and relate pixel to wavelength
+# %% read in ASP06 dark current corrected lamp measurement data and relate pixel to wavelength
 channel = "SWIR"  # set channel to work on (VNIR or SWIR)
 base = "ASP_06_Calib_Lab_20210329"
 folder_pairs = [["calib_J3_4", "Ulli_trans_J3_4"], ["calib_J5_6", "Ulli_trans_J5_6"]]
@@ -45,7 +45,7 @@ pixel_wl["S0"] = lab_calib.mean().reset_index(drop=True)  # take mean over time 
 # interpolate lamp irradiance on pixel wavelength
 lamp_func = interp1d(lamp["Wavelength"], lamp["Irradiance"], fill_value="extrapolate")
 pixel_wl["F0"] = lamp_func(pixel_wl["wavelength"])
-pixel_wl["c_lab"] = pixel_wl["F0"] / pixel_wl["S0"]  # calculate lab calibration factor
+pixel_wl["c_lab"] = pixel_wl["F0"] / pixel_wl["S0"]  # calculate lab calibration factor W/m^2/counts
 pixel_wl[pixel_wl.values < 0] = 0  # set values < 0 to 0
 
 # %% plot counts and irradiance of lamp lab calibration
@@ -53,7 +53,7 @@ fig, ax = plt.subplots()
 ax.plot(pixel_wl["wavelength"], pixel_wl["F0"], color="orange", label="Irradiance")
 ax.set_title(f"1000W Lamp Laboratory Calibration of {spectrometer}")
 ax.set_xlabel("Wavelength (nm)")
-ax.set_ylabel("Irradiance $(W\\,m^{-2})$")
+ax.set_ylabel("Irradiance (W$\\,$m$^{-2}$)")
 ax2 = ax.twinx()
 ax2.plot(pixel_wl["wavelength"], pixel_wl["S0"], label="counts")
 ax2.set_ylabel("Counts")
@@ -61,9 +61,11 @@ ax2.set_ylabel("Counts")
 lines, labels = ax.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc=0)
-plt.grid()
+ax.grid()
 plt.tight_layout()
-plt.savefig(f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_lamp_lab_calib.png", dpi=100)
+figname = f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_lamp_lab_calib.png"
+plt.savefig(figname, dpi=100)
+print(f"Saved {figname}")
 plt.show()
 plt.close()
 
@@ -72,14 +74,14 @@ ulli_file = ulli_measurement[0]
 ulli = smart.read_smart_cor(f"{calib_path}/{base}/{folders[1]}", ulli_file)
 ulli[ulli.values < 0] = 0  # set negative counts to 0
 pixel_wl["S_ulli"] = ulli.mean().reset_index(drop=True)  # take mean over time of calib measurement
-pixel_wl["F_ulli"] = pixel_wl["S_ulli"] * pixel_wl["c_lab"]  # calculate irradiance measured by Ulli
+pixel_wl["F_ulli"] = pixel_wl["S_ulli"] * pixel_wl["c_lab"]  # calculate irradiance measured from Ulli
 
 # %% plot Ulli transfer measurement from laboratory
 fig, ax = plt.subplots()
 ax.plot(pixel_wl["wavelength"], pixel_wl["F_ulli"], color="orange", label="Irradiance")
 ax.set_title(f"Ulli Transfer Sphere Laboratory Calibration of {lookup[f'{direction}_{channel}']}")
 ax.set_xlabel("Wavelength (nm)")
-ax.set_ylabel("Irradiance $(W\\,m^{-2})$")
+ax.set_ylabel("Irradiance (W$\\,$m$^{-2}$)")
 ax2 = ax.twinx()
 ax2.plot(pixel_wl["wavelength"], pixel_wl["S_ulli"], label="counts")
 ax2.set_ylabel("Counts")
@@ -87,11 +89,15 @@ ax2.set_ylabel("Counts")
 lines, labels = ax.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc=0)
-plt.grid()
+ax.grid()
 plt.tight_layout()
-plt.savefig(f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_ulli_lab_calib.png", dpi=100)
+figname = f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_ulli_lab_calib.png"
+plt.savefig(figname, dpi=100)
+print(f"Saved {figname}")
 plt.show()
 plt.close()
 
 # %% save lamp and ulli measurement from lab to file
-pixel_wl.to_csv(f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_lab_calib.dat", index=False)
+csvname = f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_lab_calib.dat"
+pixel_wl.to_csv(csvname, index=False)
+print(f"Saved {csvname}")
