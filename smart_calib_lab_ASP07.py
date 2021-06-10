cb60bf020df56d01f9115a6255566714159d2241
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-10 09:59:53 +0200

    updated lab calibration of ASP07 to include panel file

--- smart_calib_lab_ASP07.py
+++ smart_calib_lab_ASP07.py
@@ -9,7 +9,8 @@
 7. read in ulli sphere measurements
 8. plot ulli measurements
 9. write dat file with all information
-author: Johannes Roettenbacher"""
+author: Johannes Roettenbacher
+"""
 # %%
 import smart
 from smart import lookup
@@ -21,15 +22,21 @@ from scipy.interpolate import interp1d
 
 # %% set paths
 raw_path, pixel_path, calib_path, data_path, plot_path = smart.set_paths()
-
-# %% read in lamp file
-lamp = smart.read_lamp_file()
-
-# %% read in ASP06/ASP07 dark current corrected lamp measurement data and relate pixel to wavelength
+panel_path = smart.get_path("panel")
+# %% read in lamp and panel files
+lamp = smart.read_lamp_file(plot=False, save_file=False, save_fig=False)
+columns = ["wavelength", "reflectance"]
+panels = dict()
+panels["VNIR"] = pd.read_csv(f"{panel_path}/panels_VIS_8_(ASP_07).dat", skiprows=15, usecols=[0, 3], names=columns,
+                             header=None, sep="\s+")
+panels["SWIR"] = pd.read_csv(f"{panel_path}/panels_PGS_4_(ASP_07).dat", skiprows=15, usecols=[0, 3], names=columns,
+                             header=None, sep="\s+")
+# %% read in ASP07 dark current corrected lamp measurement data and relate pixel to wavelength
 channel = "SWIR"  # set channel to work on (VNIR or SWIR)
-base = "ASP_06_Calib_Lab_20210329"
-folder_pairs = [["calib_J3_4", "Ulli_trans_J3_4"], ["calib_J5_6", "Ulli_trans_J5_6"]]
-folders = folder_pairs[1]  # which folder pair to work on (0 or 1)
+base = "ASP_07_Calib_Lab_20210318"
+panel = panels[channel]
+panel["pixel"] = np.flip(panel.index + 1)
+folders = ["Cal_Plate", "Ulli_trans"]
 dirpath = os.path.join(calib_path, base, folders[0])
 dirpath_ulli = os.path.join(calib_path, base, folders[1])
 lamp_measurement = [f for f in os.listdir(dirpath) if f.endswith(f"{channel}_cor.dat")]
@@ -40,31 +47,35 @@ lab_calib = smart.read_smart_cor(dirpath, filename)
 # set negative counts to 0
 lab_calib[lab_calib.values < 0] = 0
 
-# %% read in pixel to wavelength file
+# %% add counts, lamp irradiance and lamp irradiance multiplied with panel reflectance to data frame
 spectrometer = lookup[f"{direction}_{channel}"]
-pixel_wl = smart.read_pixel_to_wavelength(pixel_path, spectrometer)
-pixel_wl["S0"] = lab_calib.mean().reset_index(drop=True)  # take mean over time of calib measurement
+panel["S0"] = lab_calib.mean().reset_index(drop=True)  # take mean over time of calib measurement
 # interpolate lamp irradiance on pixel wavelength
 lamp_func = interp1d(lamp["Wavelength"], lamp["Irradiance"], fill_value="extrapolate")
-pixel_wl["F0"] = lamp_func(pixel_wl["wavelength"])
-pixel_wl["c_lab"] = pixel_wl["F0"] / pixel_wl["S0"]  # calculate lab calibration factor
-pixel_wl[pixel_wl.values < 0] = 0  # set values < 0 to 0
+panel["F0"] = lamp_func(panel["wavelength"])
+panel["F_ref"] = panel["F0"] * panel["reflectance"]  # this is the actual measurable radiance
+panel["c_lab"] = panel["F_ref"] / panel["S0"]  # calculate lab calibration factor W/m^2/sr/counts
+panel[panel.values < 0] = 0  # set values < 0 to 0
 
-# %% plot counts and irradiance of lamp lab calibration
+# %% plot counts and radiance of lamp lab calibration
 fig, ax = plt.subplots()
-ax.plot(pixel_wl["wavelength"], pixel_wl["F0"], color="orange", label="Irradiance")
-ax.set_title(f"1000W Lamp Laboratory Calibration of {spectrometer}")
+ax.plot(panel["wavelength"], panel["F_ref"], color="orange", label="Reflected Radiance")
+ax.set_title(f"1000W Lamp and Reflectance Panel Laboratory Calibration \n"
+             f"{date_str.replace('_', '-')} {spectrometer} {direction} {channel}")
 ax.set_xlabel("Wavelength (nm)")
-ax.set_ylabel("Irradiance $(W\\,m^{-2})$")
+ax.set_ylabel("Radiance (W$\\,$sr$^{-1}\\,$m$^{-2}$)")
 ax2 = ax.twinx()
-ax2.plot(pixel_wl["wavelength"], pixel_wl["S0"], label="counts")
+ax2.plot(panel["wavelength"], panel["S0"], label="Counts")
 ax2.set_ylabel("Counts")
 # ask matplotlib for the plotted objects and their labels
 lines, labels = ax.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc=0)
-plt.grid()
-plt.savefig(f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_lamp_lab_calib.png", dpi=100)
+ax.grid()
+plt.tight_layout()
+figname = f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_lamp_lab_calib.png"
+plt.savefig(figname, dpi=100)
+print(f"Saved {figname}")
 plt.show()
 plt.close()
 
@@ -72,48 +83,31 @@ plt.close()
 ulli_file = ulli_measurement[0]
 ulli = smart.read_smart_cor(f"{calib_path}/{base}/{folders[1]}", ulli_file)
 ulli[ulli.values < 0] = 0  # set negative counts to 0
-pixel_wl["S_ulli"] = ulli.mean().reset_index(drop=True)  # take mean over time of calib measurement
-pixel_wl["F_ulli"] = pixel_wl["S_ulli"] * pixel_wl["c_lab"]  # calculate irradiance measured by Ulli
+panel["S_ulli"] = ulli.mean().reset_index(drop=True)  # take mean over time of calib measurement
+panel["F_ulli"] = panel["S_ulli"] * panel["c_lab"]  # calculate radiance measured from Ulli
 
 # %% plot Ulli transfer measurement from laboratory
 fig, ax = plt.subplots()
-ax.plot(pixel_wl["wavelength"], pixel_wl["F_ulli"], color="orange", label="Irradiance")
-ax.set_title(f"Ulli Transfer Sphere Laboratory Calibration of {lookup[f'{direction}_{channel}']}")
+ax.plot(panel["wavelength"], panel["F_ulli"], color="orange", label="Radiance")
+ax.set_title(f"Ulli Transfer Sphere Laboratory Calibration\n{spectrometer} {direction} {channel}")
 ax.set_xlabel("Wavelength (nm)")
-ax.set_ylabel("Irradiance $(W\\,m^{-2})$")
+ax.set_ylabel("Radiance (W$\\,$sr$^{-1}\\,$m$^{-2}$)")
 ax2 = ax.twinx()
-ax2.plot(pixel_wl["wavelength"], pixel_wl["S_ulli"], label="counts")
+ax2.plot(panel["wavelength"], panel["S_ulli"], label="Counts")
 ax2.set_ylabel("Counts")
 # ask matplotlib for the plotted objects and their labels
 lines, labels = ax.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc=0)
-plt.grid()
-plt.savefig(f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_ulli_lab_calib.png", dpi=100)
+ax.grid()
+plt.tight_layout()
+figname = f"{plot_path}/{date_str}_{spectrometer}_{direction}_{channel}_ulli_lab_calib.png"
+plt.savefig(figname, dpi=100)
+print(f"Saved {figname}")
 plt.show()
 plt.close()
 
-# %% save lamp and ulli measurement from lab to file
-pixel_wl.to_csv(f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_lab_calib.dat", index=False)
-
-# %% read in Ulli transfer measurement from field
-field_folder = "ASP_06_transfer_calib_20210604"
-t_int = 500
-field_file = "2021_06_04_13_40.Fdw_VNIR_cor.dat"
-ulli_field = smart.read_smart_cor(f"{calib_path}/{field_folder}/Tint_{t_int}ms", field_file)
-ulli_field[ulli_field.values < 0] = 0  # set negative counts to 0
-pixel_wl["S_ulli_field"] = ulli_field.mean().reset_index(drop=True)  # take mean over time of calib measurement
-pixel_wl["c_field"] = pixel_wl["F_ulli"] / pixel_wl["S_ulli_field"]  # calculate field calibration factor
-
-# calculate relation between S_ulli_lab and S_ulli_field
-pixel_wl["rel_ulli"] = pixel_wl["S_ulli"] / pixel_wl["S_ulli_field"]
-
-# read in dark current corrected measurement files
-measurement = smart.read_smart_cor(f"{data_path}/flight_00", "2021_03_29_11_15.Fdw_VNIR_cor.dat")
-# set negative values to 0
-measurement[measurement.values < 0] = 0
-# convert to long format
-m_long = measurement.melt(var_name="pixel", value_name="counts", ignore_index=False)
-df = m_long.merge(pixel_wl.loc[:, ["pixel", "c_field"]], on="pixel", right_index=True)
-df["F_x"] = df["counts"] * df["c_field"]
-df.pivot(columns="pixel", values="F_x")
+# %% save lamp and panel and ulli measurement from lab to file
+csvname = f"{calib_path}/{date_str}_{spectrometer}_{direction}_{channel}_lab_calib.dat"
+panel.to_csv(csvname, index=False)
+print(f"Saved {csvname}")
