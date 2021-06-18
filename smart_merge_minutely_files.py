813ecc98fec70d7783d4c14425eca7369e2dd749
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-18 22:21:32 +0200

    updated paths

--- smart_merge_minutely_files.py
+++ smart_merge_minutely_files.py
@@ -14,8 +14,8 @@ import pandas as pd
 
 # combine minutely files from ASP_07 to one file
 raw_path, _, calib_path, data_path, _ = set_paths()
-directory = raw_path
-folder = "flight_00"
+directory = calib_path
+folder = "ASP06_transfer_calib_20210616"
 channels = ["SWIR", "VNIR"]
 property = ["Iup", "Fup", "Fdw"]
 for dirpath, dirs, files in os.walk(os.path.join(directory, folder)):
