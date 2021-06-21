49c04893449245db380d026f93a67bacb293bbf7
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-06-21 14:18:06 +0200

    added logging and adjusted file listing to dark current corrected files (_cor)

--- smart_merge_minutely_files.py
+++ smart_merge_minutely_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-ASP_07 was configured to write minutely files during calibration.
+ASP06 and ASP07 were configured to write minutely files during calibration.
 This script
 * merges the minutely files into one file,
 * deletes the minutely files,
@@ -11,24 +11,29 @@ author: Johannes Röttenbacher
 from smart import set_paths
 import os
 import pandas as pd
+import logging
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
 
 # combine minutely files from ASP_07 to one file
 raw_path, _, calib_path, data_path, _ = set_paths()
-directory = calib_path
-folder = "ASP06_transfer_calib_20210616"
+directory = data_path
+folder = "flight_00"
 channels = ["SWIR", "VNIR"]
 property = ["Iup", "Fup", "Fdw"]
 for dirpath, dirs, files in os.walk(os.path.join(directory, folder)):
     for prop in property:
         for channel in channels:
             try:
-                filename = [file for file in files if file.endswith(f"{prop}_{channel}.dat")]
+                filename = [file for file in files if file.endswith(f"{prop}_{channel}_cor.dat")]
                 df = pd.concat([pd.read_csv(f"{dirpath}/{file}", sep="\t", header=None) for file in filename])
                 # delete all minutely files
                 for file in filename:
                     os.remove(os.path.join(dirpath, file))
                 outname = f"{dirpath}/{filename[0]}"
                 df.to_csv(outname, sep="\t", index=False, header=False)
-                print(f"Saved {outname}")
+                log.info(f"Saved {outname}")
             except ValueError:
                 pass
