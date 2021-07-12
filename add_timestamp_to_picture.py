69801b328f837fb67d03b63474fe845179cbd621
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-12 08:22:19 +0200

    script to add timestamp to each gopro picture calling magick convert

---
+++ add_timestamp_to_picture.py
@@ -0,0 +1,13 @@
+#!\usr\bin\env python
+"""Add the timstamp to a picture
+author: Johannes Röttenbacher
+"""
+
+import os
+from tqdm import tqdm
+
+path = "/mnt/c/Users/Johannes/Documents/Gopro/20210710"
+files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".JPG")]
+
+for f in tqdm(files):
+    os.system(f"convert {f} -pointsize 72 -fill black -annotate +3200+2900  %[exif:DateTimeOriginal] {f}")
