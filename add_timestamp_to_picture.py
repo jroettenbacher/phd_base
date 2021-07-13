392324ed1cedd8167f1c9eb3078255c5f27ee198
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-13 08:15:01 +0200

    added print output

--- add_timestamp_to_picture.py
+++ add_timestamp_to_picture.py
@@ -6,8 +6,10 @@ author: Johannes Röttenbacher
 import os
 from tqdm import tqdm
 
-path = "/mnt/c/Users/Johannes/Documents/Gopro/20210710"
+path = "/mnt/d/CIRRUS-HL/Gopro/20210703"
 files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".JPG")]
 
 for f in tqdm(files):
     os.system(f"convert {f} -pointsize 72 -fill black -annotate +3200+2900  %[exif:DateTimeOriginal] {f}")
+
+print(f"Done with all files in {path}")
