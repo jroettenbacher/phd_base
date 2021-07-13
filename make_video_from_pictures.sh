fd3c025f8fab514bad111b75f118ed37df783fa2
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-13 08:16:18 +0200

    added base_dir and some options

--- make_video_from_pictures.sh
+++ make_video_from_pictures.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # create a stop motion video from GoPro time laps pictures
-date="20210623"
-inpath="/mnt/c/Users/Johannes/Documents/Gopro/${date}"
+date="20210708"
+base_dir="/mnt/d/CIRRUS-HL/Gopro"
+#inpath="/mnt/c/Users/Johannes/Documents/Gopro/${date}_2"
+inpath="${base_dir}/${date}"
 outfile="${date}_Gopro_video.mp4"
-outpath="/mnt/c/Users/Johannes/Documents/Gopro/${outfile}"
+outpath="${base_dir}/${outfile}"
 
-ffmpeg -i ${inpath}/${date}_Gopro_%%04d.JPG -vcodec libx264 -b:v 50000k -r 6 ${outpath}
+ffmpeg -framerate 12 -f image2 -i ${inpath}/${date}_Gopro_%04d.JPG -vcodec libx264 -b:v 50000k -r 12 ${outpath}
 
-# options: -i -> filename, %%04d -> 0001 ... 9999, -b:v -> video bitrate, -r 6 -> frames per second
+# options: -f -> filetype, -i -> filename, %%04d -> 0001 ... 9999, -b:v -> video bitrate, -r 6 -> frames per second
