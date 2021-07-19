f35167d115e24f0cfa55576a382be582b9b51842
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-19 15:59:08 +0200

    added more variables to set, made the call more readable

--- make_video_from_pictures.sh
+++ make_video_from_pictures.sh
@@ -2,12 +2,17 @@
 
 # create a stop motion video from GoPro time laps pictures
 date="20210708"
-base_dir="/mnt/d/CIRRUS-HL/Gopro"
-#inpath="/mnt/c/Users/Johannes/Documents/Gopro/${date}_2"
-inpath="${base_dir}/${date}"
-outfile="${date}_Gopro_video.mp4"
+#base_dir="/mnt/d/CIRRUS-HL/Gopro"
+base_dir="/mnt/c/Users/Johannes/Documents/Gopro"
+inpath="${base_dir}/${date}_short2"
+outfile="${date}_short2_Gopro_video.mp4"
 outpath="${base_dir}/${outfile}"
-
-ffmpeg -framerate 12 -f image2 -i ${inpath}/${date}_Gopro_%04d.JPG -vcodec libx264 -b:v 50000k -r 12 ${outpath}
+framerate="12"
+start_number="720"
+ffmpeg -framerate ${framerate} -f image2 \
+  -start_number ${start_number} \
+  -i ${inpath}/${date}_Gopro_%04d.JPG \
+  -vcodec libx264 -b:v 50000k \
+  -r ${framerate} ${outpath}
 
 # options: -f -> filetype, -i -> filename, %%04d -> 0001 ... 9999, -b:v -> video bitrate, -r 6 -> frames per second
