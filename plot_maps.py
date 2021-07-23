cd11e5fcd1907b786368bd1f34b06dcf9723efec
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-23 17:00:37 +0200

    added dictionary for plotting options for each flight

--- plot_maps.py
+++ plot_maps.py
@@ -23,15 +23,20 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.WARNING)
 
 # %% set paths
-date = 20210625
+date = 20210628
 flight = f"Flight_{date}a"
 bahamas_dir = smart.get_path("bahamas")
 bahamas_path = f"{bahamas_dir}/{flight}"
-gopro_dir = f"C:/Users/Johannes/Documents/Gopro"
+gopro_dir = smart.get_path("gopro")
 # find bahamas file
 file = [f for f in os.listdir(bahamas_path) if f.endswith(".nc")][0]
 # select second airport for map plot according to flight
 airport = stop_over_locations[flight] if flight in stop_over_locations else None
+
+# %% set plotting options for each flight
+plot_props = dict(Flight_20210625a=dict(figsize=(9, 9), cb_loc="left", shrink=1, l_loc=1),
+                  Flight_20210626a=dict(figsize=(9.5, 8), cb_loc="bottom", shrink=0.9, l_loc=4),
+                  Flight_20210628a=dict(figsize=(10, 9), cb_loc="left", shrink=1, l_loc=4))
 # %% read in bahamas data
 bahamas = smart.read_bahamas(f"{bahamas_path}/{file}")
 # select only position data
@@ -39,7 +44,7 @@ lon = bahamas["IRS_LON"]
 lat = bahamas["IRS_LAT"]
 
 # %% find map extend
-pad = 2
+pad = 1
 llcrnlat = lat.min(skipna=True) - pad
 llcrnlon = lon.min(skipna=True) - pad
 urcrnlat = lat.max(skipna=True) + pad
@@ -90,7 +95,9 @@ def plot_bahamas_map(flight: str, lon, lat, extent: list, lon1: float, lat1: flo
     font = {'weight': 'bold', 'size': 26}
     matplotlib.rc('font', **font)
 
-    fig, ax = plt.subplots(figsize=(11, 8), subplot_kw={"projection": ccrs.PlateCarree()})
+    # get plot properties
+    props = plot_props[flight]
+    fig, ax = plt.subplots(figsize=props["figsize"], subplot_kw={"projection": ccrs.PlateCarree()})
     ax.stock_img()
     ax.coastlines()
     ax.add_feature(cartopy.feature.BORDERS)
@@ -114,9 +121,9 @@ def plot_bahamas_map(flight: str, lon, lat, extent: list, lon1: float, lat1: flo
         ax.text(x2 + 0.1, y2 + 0.1, airport, fontsize=16)
     # plot flight track and color by flight altitude
     points = ax.scatter(lon, lat, c=bahamas.IRS_ALT/1000, s=10)
-    # add the corresponding colorbar
-    plt.colorbar(points, ax=ax, pad=0.01, orientation="horizontal", label="Height (km)", shrink=0.89)
-    ax.legend(loc=1)
+    # add the corresponding colorbar and decide whether to plot it horizontally or vertically
+    ax.legend(loc=props["l_loc"])
+    plt.colorbar(points, ax=ax, pad=0.01, location=props["cb_loc"], label="Height (km)", shrink=props["shrink"])
     plt.tight_layout(pad=0.1)
     fig_name = f"{outpath}/{flight}_map_{number:04}.png"
     plt.savefig(fig_name, dpi=100)
@@ -128,7 +135,7 @@ def plot_bahamas_map(flight: str, lon, lat, extent: list, lon1: float, lat1: flo
 # lon1 = lon[0]
 # lat1 = lat[0]
 # number = 0
-# plot_bahamas_map(flight, lon, lat, extent, lon1, lat1, number, airport="EDMO")
-Parallel(n_jobs=cpu_count()-2)(delayed(plot_bahamas_map)
-                               (flight, lon, lat, extent, lon1, lat1, number, airport=airport)
-                               for lon1, lat1, number in zip(tqdm(lon_sel), lat_sel, ts_sel.number.values))
+# plot_bahamas_map(flight, lon, lat, extent, lon1, lat1, number, airport=airport)
+# Parallel(n_jobs=cpu_count()-2)(delayed(plot_bahamas_map)
+#                                (flight, lon, lat, extent, lon1, lat1, number, airport=airport)
+#                                for lon1, lat1, number in zip(tqdm(lon_sel), lat_sel, ts_sel.number.values))
