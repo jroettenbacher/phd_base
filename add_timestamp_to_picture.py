eb32fa7bdd3de1e154195059b65fb7f9f6df2ecf
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-07-23 13:26:08 +0200

    added correction for time offset from BAHAMAS

--- add_timestamp_to_picture.py
+++ add_timestamp_to_picture.py
@@ -1,25 +1,40 @@
 #!\usr\bin\env python
 """Add the timstamp to a picture (run on Ubuntu)
+corrects the meta data timestamp in an image for a
 author: Johannes Röttenbacher
 """
 
 # %%
 import os
+import datetime
 from tqdm import tqdm
 from subprocess import run, Popen
+from smart import gopro_lt, gopro_offsets
 
-# path = "/mnt/e/CIRRUS-HL/Gopro/20210629"
-path = "/mnt/c/Users/Johannes/Documents/Gopro/20210712"
+flight = "Flight_20210625"
+# file = "/mnt/c/Users/Johannes/Documents/Gopro/20210625_Gopro_0001.JPG"
+# path = "/mnt/e/CIRRUS-HL/Gopro/{flight[7:]}"
+path = f"/mnt/c/Users/Johannes/Documents/Gopro/{flight[7:]}"
 correct_time = True
+sync_to_bahamas = True if flight in gopro_offsets else False
+LT_to_UTC = gopro_lt[flight] if flight in gopro_lt else False
 start_file = 0
 files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".JPG")][start_file:]
 processes = set()
 max_processes = 10
 
-# %% update meta data time stamp to set to UTC (GoPro switched to LT on 28.06 due to WiFi connection)
+# %% update meta data time stamp to set to UTC
 if correct_time:
-    utc_correction = 2  # convert local time to UTC
-    run(['exiftool', '-m', '-progress', '-overwrite_original', f'-DateTimeOriginal-={utc_correction}', path])
+    # GoPro switched to LT on 28.06 due to WiFi connection
+    utc_correction = 2 if LT_to_UTC else 0  # convert local time to UTC
+    # GoPro is not synched to BAHAMAS
+    bahamas_correction = gopro_offsets[flight] if sync_to_bahamas else 0
+    correction = utc_correction + bahamas_correction / 60 / 60  # convert seconds to hours
+    # format correction
+    delta = datetime.timedelta(hours=abs(correction))
+    sign = "-" if correction > 0 else "+"
+    cor_str = str(delta)
+    run(['exiftool', '-m', '-progress', '-overwrite_original', f'-DateTimeOriginal{sign}={cor_str}', path])
 
 # %% add the time stamp from the exif meta data in the right lower corner
 for f in tqdm(files, desc="Convert"):
