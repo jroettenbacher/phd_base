d9ae7dbe301011c5e76379a9ac2f9dee953759cc
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-09-03 16:33:17 +0200

    Add plot for broadband irradiance

--- cirrus_hl_case_studies.py
+++ cirrus_hl_case_studies.py
@@ -5,17 +5,20 @@ author: Johannes Röttenbacher
 """
 
 # %% module import
+import datetime
 from smart import get_path
 import logging
 from bahamas import plot_props
+from libradtran import read_libradtran
 from cirrus_hl import stop_over_locations, coordinates
 import os
 import smart
-from functions_jr import make_dir
+from functions_jr import make_dir, set_cb_friendly_colors
 import matplotlib
 import matplotlib.pyplot as plt
 import cartopy
 import cartopy.crs as ccrs
+import xarray as xr
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
@@ -27,8 +30,11 @@ log.setLevel(logging.INFO)
 # %% set paths
 flight = "Flight_20210629a"
 bahamas_dir = get_path("bahamas", flight)
-outpath = f"C:/Users/Johannes/Documents/campaigns/CIRRUS-HL/case_studies/{flight}"
+bacardi_dir = get_path("bacardi", flight)
+outpath = f"C:/Users/Johannes/Documents/Doktor/campaigns/CIRRUS-HL/case_studies/{flight}"
 make_dir(outpath)
+start_dt = datetime.datetime(2021, 6, 29, 10, 10)
+end_dt = datetime.datetime(2021, 6, 29, 11, 54)
 
 # %% find bahamas file and read in bahamas data
 file = [f for f in os.listdir(bahamas_dir) if f.endswith(".nc")][0]
@@ -85,4 +91,28 @@ plt.tight_layout(pad=0.1)
 fig_name = f"{outpath}/{flight}_bahamas_track.png"
 plt.savefig(fig_name, dpi=100)
 log.info(f"Saved {fig_name}")
+plt.close()
+
+# %% read ind libradtran and bacardi files
+libradtran_file = "BBR_Fdn_clear_sky_Flight_20210629a_R0_ds_high_jr.dat"
+bacardi_file = "CIRRUS_HL_F05_20210629a_ADLR_BACARDI_BroadbandFluxes_R0.nc"
+bbr_sim = read_libradtran(flight, libradtran_file)
+bacardi_ds = xr.open_dataset(f"{bacardi_dir}/{bacardi_file}")
+
+# %% plot libradtran simulations together with BACARDI measurements
+set_cb_friendly_colors()
+fig, ax = plt.subplots()
+bacardi_ds.F_up_solar.plot(x="time", label="F_up solar BACARDI", ax=ax)
+bacardi_ds.F_down_solar.plot(x="time", label="F_dw solar BACARDI", ax=ax)
+bbr_sim.plot(y="F_dw", ax=ax, ylabel="Broadband irradiance (W$\,$m$^{-2}$)", label="F_dw solar libRadtran")
+bbr_sim.plot(y="F_up", ax=ax, label="F_up solar libRadtran")
+ax.set_xlabel("Time (UTC)")
+ax.grid()
+ax.fill_between(bbr_sim.index, 0, 1, where=((start_dt < bbr_sim.index) & (bbr_sim.index < end_dt)),
+                transform=ax.get_xaxis_transform(), label="Case Study", color="grey")
+ax.legend(bbox_to_anchor=(0.2, 0), loc="lower left", bbox_transform=fig.transFigure, ncol=2)
+plt.subplots_adjust(bottom=0.3)
+plt.tight_layout()
+plt.show()
+plt.savefig(f"{outpath}/{flight}_bacardi_libradtran_broadband_irradiance.png", dpi=100)
 plt.close()
