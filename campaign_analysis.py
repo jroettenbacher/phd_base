f9485d6f1521a4ab652e6adb00874e3dca7b4153
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-09-13 10:44:47 +0200

    Add radiosonde location plot

--- campaign_analysis.py
+++ campaign_analysis.py
@@ -6,6 +6,11 @@ author: Johannes Röttenbacher
 # %% module import
 import xarray as xr
 from smart import get_path
+import matplotlib.pyplot as plt
+from matplotlib import patheffects
+import cartopy
+import cartopy.crs as ccrs
+from cirrus_hl import coordinates
 
 # %% define functions
 
@@ -32,3 +37,30 @@ lon_max = ds.IRS_LON.max().compute().values
 alt_max = ds.IRS_ALT.max().compute().values
 print(f"Minumum Latitude: {lat_min}°N\nMaximum Latitude: {lat_max}°N\nMinumum Longitude: {lon_min}°E"
       f"\nMaximum Longitude: {lon_max}°E\nMaximum Altitutde: {alt_max}m")
+
+# %% prepare radiosonde dictionary and remove non radiosonde coordinates
+rs_coordinates = coordinates
+rs_coordinates.pop("EDMO")
+rs_coordinates.pop("Santiago")
+
+# %% plot radiosonde stations
+fig, ax = plt.subplots(figsize=(8, 5), subplot_kw={"projection": ccrs.PlateCarree()})
+ax.stock_img()
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.set_extent([-40, 30, 35, 80])
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
+gl.bottom_labels = False
+gl.left_labels = False
+# plot each radiosonde station
+for station in rs_coordinates:
+    coords = rs_coordinates[station]
+    ax.annotate(station, coords, fontsize=12,
+                path_effects=[patheffects.withStroke(linewidth=3, foreground="w")])
+    ax.plot(coords[0], coords[1], '.r', markersize=10)
+
+ax.set_title("CIRRUS-HL - Radiosonde staions close to flight paths")
+plt.tight_layout()
+# plt.show()
+plt.savefig("CIRRUS-HL_Radiosonde_stations.png", dpi=100)
+plt.close()
