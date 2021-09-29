1b920f309f717ddd4071aef55a9a5518f8057dd5
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-09-29 10:47:00 +0200

    Update to latest flight and new paths

--- make_video_from_pictures.sh
+++ make_video_from_pictures.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 # create a stop motion video from GoPro time laps pictures
-date="20210730"
+date="20210729"
 number="a"
 #base_dir="/mnt/d/CIRRUS-HL/Gopro"
-base_dir="/mnt/c/Users/Johannes/Documents/Gopro"
+base_dir="/mnt/e/CIRRUS-HL/04_Gopro"
 inpath="${base_dir}/Flight_${date}${number}"
-inpath="${base_dir}/${date}"
-outfile="${date}${number}_Gopro_video_slow.mp4"  # change name according to framrate (24=fast, 12=slow)
+outfile="${date}${number}_Gopro_video_fast.mp4"  # change name according to framrate (24=fast, 12=slow)
 outpath="${base_dir}/${outfile}"
-framerate="12"
-start_number="1"
+framerate="24"
+start_number="102"
 ffmpeg -framerate ${framerate} -f image2 \
   -start_number ${start_number} \
   -i ${inpath}/${date}_Gopro_%04d.JPG \
