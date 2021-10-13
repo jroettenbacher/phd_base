a5c602bea6542ed590f2d19c6836dafac2e49e95
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-10-13 12:07:01 +0200

    Add check if all simulations finished
    * Fix naming bug -> error_file was named log
    * introduce while loop to wait for all started simulations to finish
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- libradtran_run_uvspec.py
+++ libradtran_run_uvspec.py
@@ -30,19 +30,25 @@ error_logs = [f.replace(".out", ".log") for f in output_files]
 
 # %% call uvspec for one file
 # index = 0
-# with open(input_files[index], "r") as ifile, open(output_files[index], "w") as ofile, open(error_logs[index], "w") as log:
-#     Popen([uvspec_exe], stdin=ifile, stdout=ofile, stderr=log)
+# with open(input_files[index], "r") as ifile, open(output_files[index], "w") as ofile, open(error_logs[index], "w") as lfile:
+#     Popen([uvspec_exe], stdin=ifile, stdout=ofile, stderr=lfile)
 
 # %% call uvspec for all files
 
 processes = set()
 max_processes = cpu_count() - 4
 for infile, outfile, log_file in zip(tqdm(input_files, desc="libRadtran simulations"), output_files, error_logs):
-    with open(infile, "r") as ifile, open(outfile, "w") as ofile, open(log_file, "w") as log:
-        processes.add(Popen([uvspec_exe], stdin=ifile, stdout=ofile, stderr=log))
+    with open(infile, "r") as ifile, open(outfile, "w") as ofile, open(log_file, "w") as lfile:
+        processes.add(Popen([uvspec_exe], stdin=ifile, stdout=ofile, stderr=lfile))
     if len(processes) >= max_processes:
         os.wait()
         processes.difference_update([p for p in processes if p.poll() is not None])
+
+# wait for all simulations to finish
+while len(processes) > 0:
+    # this will remove elements of the set which are also in the list
+    # the list has only terminated processes in it, p.poll returns a non None value if the process is still running
+    processes.difference_update([p for p in processes if p.poll() is not None])
 
 # %% merge output files and write a netCDF file
 
