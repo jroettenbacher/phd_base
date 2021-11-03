3297afc27458958596cadf5fdee47a3b642d80d4
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-03 13:44:35 +0100

    Adjust script to new project structure
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- analysis/cirrus_hl_overview.py
+++ analysis/cirrus_hl_overview.py
@@ -4,39 +4,30 @@ author: Johannes Röttenbacher
 """
 
 # %% module import
+import pylim.helpers as h
+from pylim.cirrus_hl import flight_hours
+from pylim.bahamas import plot_bahamas_flight_track, preprocess_bahamas
+from pylim.reader import read_bahamas
 import numpy as np
-from smart import get_path
-import logging
-from bahamas import plot_props, read_bahamas, plot_bahamas_flight_track
-from libradtran import read_libradtran
-from cirrus_hl import stop_over_locations, coordinates, flight_hours
+import pandas as pd
 import os
 from geopy.distance import geodesic
-import smart
-from helpers import make_dir, set_cb_friendly_colors, set_xticks_and_xlabels
-import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import patheffects
-from matplotlib.patches import Patch
-import cartopy
-import cartopy.crs as ccrs
 import xarray as xr
-import pandas as pd
-import rasterio
-from rasterio.plot import show
-
-log = logging.getLogger(__name__)
+import logging
+log = logging.getLogger("pylim")
 log.addHandler(logging.StreamHandler())
 log.setLevel(logging.INFO)
+
 # %% set paths
 flight = "Flight_20210625a"
-bahamas_dir = get_path("bahamas", flight)
-bahamas_dir_all = get_path("all", instrument="BAHAMAS")
-bacardi_dir = get_path("bacardi", flight)
-smart_dir = get_path("calibrated", flight)
+bahamas_dir = h.get_path("bahamas", flight)
+bahamas_dir_all = h.get_path("all", instrument="BAHAMAS")
+bacardi_dir = h.get_path("bacardi", flight)
+smart_dir = h.get_path("calibrated", flight)
 sat_image = "/projekt_agmwend/data/Cirrus_HL/01_Flights/Flight_20210629a/satellite/snapshot-2021-06-29T00_00_00Z.tiff"
 outpath = f"C:/Users/Johannes/Documents/Doktor/campaigns/CIRRUS-HL/overview"
-make_dir(outpath)
+h.make_dir(outpath)
 # read in BAHAMAS and BACARDI data
 file = [f for f in os.listdir(bahamas_dir) if f.endswith(".nc")][0]
 bahamas = read_bahamas(flight)
@@ -49,7 +40,7 @@ plot_bahamas_flight_track(flight, outpath=outpath)
 total_flight_hours = np.sum(flight_hours).total_seconds() / 60 / 60
 
 # %% plot bahamas heading and solar azimuth angle
-set_cb_friendly_colors()
+h.set_cb_friendly_colors()
 fig, ax = plt.subplots()
 bahamas.IRS_HDG.plot(x="TIME", label="BAHAMAS Heading", ax=ax)
 bacardi.saa.plot(x="time", label="Solar Azimuth Angle", ax=ax)
@@ -61,14 +52,6 @@ plt.show()
 plt.close()
 
 # %% read in all BAHAMAS data
-
-
-def preprocess_bahamas(ds: xr.Dataset) -> xr.Dataset:
-    ds = ds.swap_dims({"tid": "TIME"})
-    ds = ds.rename({"TIME": "time"})
-    return ds
-
-
 bahamas_all_files = [os.path.join(bahamas_dir_all, f) for f in os.listdir(bahamas_dir_all)]
 bahamas_all = xr.open_mfdataset(bahamas_all_files, preprocess=preprocess_bahamas)
 
