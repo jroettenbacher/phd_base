d3c57c2e834f8c3deaa5fe3b25524867beb6a4c0
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-11 17:16:37 +0100

    Implement new dark current correction for SWIR
    Try to use the diffuse measurement to the corresponding angle and take the mean of that measurement to correct the dark current of the current file. If now diffuse measurement is available, take the closest available folder and search for a file there. If that does not work loop through all diffuse folders until a file is found. -> To be replaced by the 0° measurement
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- analysis/smart_cosine_calibration.py
+++ analysis/smart_cosine_calibration.py
@@ -13,7 +13,9 @@ author: Johannes Röttenbacher
 # %% module import
 import pylim.helpers as h
 from pylim import reader, smart
+import sys
 import os
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 from copy import deepcopy
@@ -58,22 +60,76 @@ log.info("Merged all diffuse VNIR files")
 # if no such measurement is available use the first 70 pixel from each measurement as dark current (option 1)
 # or the 95deg diffuse measurement (option 2). However, this has the problem that it was probably warmer during that
 # measurement than during the one which is being corrected -> higher dark current at 95deg
-option = 1
+# the shutters on the SWIR spectrometers did not work -> use the diffuse measurements also for the correction of the
+# SWIR measured dark current -> SWIR_option = 2
+VNIR_option = 1
+SWIR_option = 2
 for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
     log.debug(f"Working on {dirpath}")
     dirname = os.path.basename(dirpath)
+    parent = os.path.dirname(dirpath)
     for file in files:
         if file.endswith("SWIR.dat"):
-            log.debug(f"Working on {dirpath}/{file}")
-            smart_cor = smart.correct_smart_dark_current("", file, option=2, path=dirpath)
-            outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
-            smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
-            log.debug(f"Saved {outname}")
+            if SWIR_option == 1:
+                # use shutter measurements to correct each file
+                log.debug(f"Working on {dirpath}/{file}")
+                smart_cor = smart.correct_smart_dark_current("", file, option=2, path=dirpath)
+
+            else:
+                assert SWIR_option == 2, f"Wrong option {SWIR_option} provided!"
+                # use diffuse measurement to correct SWIR dark current if possible else use 0° measurement
+                log.debug(f"Working on {dirpath}/{file}")
+                _, channel, direction = smart.get_info_from_filename(file)
+                try:
+                    new_dirname = dirname.replace(dirname, f"{dirname}_diffuse") if "diffuse" not in dirname else dirname
+                    dark_dir = os.path.join(parent, new_dirname)
+                    dark_file = [f for f in os.listdir(dark_dir) if direction in f and channel in f][0]
+
+                except (FileNotFoundError, IndexError):
+                    log.debug(f"No diffuse {direction}_{channel} file found in {dark_dir}")
+                    dark_file = []  # initiate empty list
+                    count = 0
+                    while len(dark_file) == 0:
+                        # loop through diffuse folders and take the one closest to the current angle
+                        if "turned" in dirname:
+                            diffuse_folders = [d for d in os.listdir(parent) if "diffuse" in d and "turned" in d]
+                        else:
+                            diffuse_folders = [d for d in os.listdir(parent) if "diffuse" in d and "turned" not in d]
+
+                        available_angles = [float(re.search(r"-?\d{1,2}", d)[0]) for d in diffuse_folders]
+                        current_angle = float(re.search(r"-?\d{1,2}", dirname)[0])  # get angle from dirname
+                        closest_angle = available_angles[h.arg_nearest(available_angles, current_angle)]
+                        # not every channel can be found in every folder
+                        # -> increase/decrease closest angle by 5 in each loop (alternate between increase and decrease)
+                        if count % 2 == 0:
+                            closest_angle = closest_angle + count / 2 * 5
+                        else:
+                            closest_angle = closest_angle - np.ceil(count / 2) * 5
+                        # generate new dark directory according to closest angle
+                        new_dirname = dirname.replace(dirname, f"{dirname}_diffuse").replace(str(int(current_angle)),
+                                                                                             str(int(closest_angle)))
+                        dark_dir = os.path.join(parent, new_dirname)
+                        try:
+                            dark_file = [f for f in os.listdir(dark_dir) if direction in f and channel in f][0]
+                        except (FileNotFoundError, IndexError):
+                            count += 1
+                            log.debug(f"No {direction}_{channel} file found in {dark_dir}\n count: {count}")
+                            if count > 40:
+                                sys.exit(1)
+                            continue
+
+                measurement = reader.read_smart_raw(dirpath, file)
+                dark_current = reader.read_smart_raw(dark_dir, dark_file)
+                dark_current = dark_current.iloc[:, 2:].mean()
+                # only use data when shutter is open
+                measurement = measurement.where(measurement.shutter == 1).iloc[:, 2:]
+                smart_cor = measurement - dark_current
 
         if file.endswith("VNIR.dat"):
             log.debug(f"Working on {dirpath}/{file}")
             _, channel, direction = smart.get_info_from_filename(file)
-            dark_dir = dirpath.replace(dirname, f"{dirname}_diffuse")
+            new_dirname = dirname.replace(dirname, f"{dirname}_diffuse")
+            dark_dir = os.path.join(parent, new_dirname)
             try:
                 dark_file = [f for f in os.listdir(dark_dir) if direction in f and channel in f][0]
                 measurement = reader.read_smart_raw(dirpath, file)
@@ -83,8 +139,8 @@ for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
                 measurement = measurement.where(measurement.shutter == 1).iloc[:, 2:]
                 smart_cor = measurement - dark_current
             except (FileNotFoundError, IndexError):
-                log.debug(f"No diffuse file for direction: {direction} and channel: {channel} can be found")
-                if option == 1:
+                log.debug(f"No diffuse {direction}_{channel} file found in {dark_dir}")
+                if VNIR_option == 1:
                     smart_cor = smart.correct_smart_dark_current("", file, option=1, path=dirpath)
                 else:
                     dark_dir = f"{calib_path}/{folder}/95_turned_diffuse"
@@ -95,10 +151,13 @@ for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
                     # only use data when shutter is open
                     measurement = measurement.where(measurement.shutter == 1).iloc[:, 2:]
                     smart_cor = measurement - dark_current
+        else:
+            # skip all _cor.dat files
+            continue
 
-            outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
-            smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
-            log.debug(f"Saved {outname}")
+        outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
+        smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
+        log.debug(f"Saved {outname}")
 
 log.info("Corrected all raw files for the dark current")
 # merge minutely corrected files to one file
