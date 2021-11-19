ef1fafc9afe5977645f48b38569e1b77e352d23a
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-19 13:25:36 +0100

    Update pip install
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -18,7 +18,7 @@ set -x
 
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
-pip install myst_parser
+pip3 install myst_parser
 
 #####################
 # DECLARE VARIABLES #
