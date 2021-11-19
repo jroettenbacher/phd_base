dfd67e45f0369e7108c76684863a0bb29cdb1db6
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-19 14:23:35 +0100

    Reorder myst_parser install
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -17,7 +17,8 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip python3-myst_parser
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
+pip3 install myst_parser
 pip3 install -r requirements.txt
 
 #####################
