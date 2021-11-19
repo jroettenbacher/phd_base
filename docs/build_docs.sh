a55048ee7979dbb72e065f9f7118aa6bf723bf71
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-19 14:00:23 +0100

    Bug fix
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -18,7 +18,7 @@ set -x
 
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
-pip3 install myst_parser python=3.8
+pip3 install myst_parser python==3.8
 pip3 install -r requirements.txt
 
 #####################
