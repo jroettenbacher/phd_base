1094acc5dcbbb3f903f9c87995f904b011275a5a
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-11-29 14:20:07 +0100

    Add first try at writing SMART to netCDF
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/smart_write_ncfile.py
+++ processing/smart_write_ncfile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Given a SMART input file write a well documented netCDF file.
+
 Two options:
 
     1. One spectrometer = one file
@@ -11,10 +12,63 @@ Go with option 1 for now.
 
 The netCDF file could be writen as a standard output from smart_calibrate_measurement.py or as a separate step in this
  script. Start with this script and write a function that can then be used in the calibration script.
+
 author: Johannes Röttenbacher
 """
+
 if __name__ == "__main__":
+    # %% module import
     import pylim.helpers as h
     from pylim import reader
+    from pylim import cirrus_hl, smart
+    import os
+    import pandas as pd
+    import logging
 
-    flight = "Flight_20210624a"
+    # %% set up logging
+    log = logging.getLogger("pylim")
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.INFO)
+
+    # %% set user variables
+    campaign = "cirrus-hl"
+    flight = "Flight_20210629a"
+    prop_channel = "Fdw_VNIR"
+    t_int = 300
+
+    # %% get paths and read in files
+    smart_dir = h.get_path("calibrated", flight=flight, campaign=campaign)
+    pixel_wl_dir = h.get_path("pixel_wl", campaign=campaign)
+    smart_file = [f for f in os.listdir(smart_dir) if prop_channel in f][0]
+    # read in smart calibrated data
+    cal_data = reader.read_smart_cor(smart_dir, smart_file)
+    # read in pixel to wavelength mapping
+    pixel_wl = reader.read_pixel_to_wavelength(pixel_wl_dir, cirrus_hl.lookup[prop_channel])
+
+    # %% set negative values to 0
+    cal_data[cal_data < 0] = 0
+
+    # %% prepare dataframe for conversion to xr.Dataset
+    cal_data_long = pd.melt(cal_data, var_name="pixel", value_name="irradiance", ignore_index=False)  # convert to long
+    # merge wavelength to data and set it as a multi index together with time
+    cal_data_long = cal_data_long.reset_index().merge(pixel_wl, how="left", on="pixel").set_index(["time", "wavelength"])
+
+    ds = cal_data_long.to_xarray()
+
+    # %% multiply normalized irradiance with integration time
+    ds["irradiance"] = ds["irradiance"] * t_int
+
+    # %% create metadata for ncfile
+    ds.irradiance.attrs = dict(long_name="spectral downward irradiance", units="W m-2")
+    ds = ds.rename(dict(irradiance=prop_channel[:3]))
+    ds["pixel"] = ds.pixel.isel(time=0, drop=True)
+    # TODO: Add global attributes and standard name
+    # TODO: create attributes according to prop_channel
+
+    # %% create ncfile
+    date_str, prop, direction = smart.get_info_from_filename(smart_file)
+    outfile = f"{campaign}_SMART_{direction}_{prop}_{date_str}.nc"
+    outpath = os.path.join(smart_dir, outfile)
+    ds.to_netcdf(outpath, format="NETCDF4_CLASSIC",
+                 encoding={"time": {"units": "seconds since 2021-01-01"}})
+    log.info(f"Saved {outpath}")
