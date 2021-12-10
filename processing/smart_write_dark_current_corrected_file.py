a0250c1d552722566238ed55412a9c831eb0148c
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-12-10 12:57:21 +0100

    Restructure skript
    - Adjust docstring
    - Move function before main frame
    - Use new option 3 for VNIR files (scaled dark current from transfer calib)
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/smart_write_dark_current_corrected_file.py
+++ processing/smart_write_dark_current_corrected_file.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
-"""Script to correct SMART measurement for dark current and save it to a new file
-input: raw smart measurements
-output: dark current corrected smart measurements
-author: Johannes Roettenbacher
+"""Script to correct all SMART measurements from one flight for the dark current and save them to new files
+
+**input**: raw smart measurements
+
+**output**: dark current corrected smart measurements
+
+*author*: Johannes Roettenbacher
 """
+
+
+def make_dark_cur_cor_file(flight: str, file: str, inpath: str, transfer_cali_date: str, outdir: str) -> None:
+    """
+    Function to write a dark current corrected file to the given input file
+    Args:
+        flight: to which flight does the file belong to? (e.g. Flight_20210707a)
+        file: Standard SMART file name
+        inpath: input directory
+        transfer_cali_date: date of transfer calibration to use for the dark current measurements ("" or yyyymmdd)
+        outdir: output directory
+
+    Returns: Writes dark current corrected file to outdir
+
+    """
+    log.info(f"Working on {file}")
+    if len(transfer_cali_date) > 0:
+        smart_cor = smart.correct_smart_dark_current(flight, file, option=3, path=inpath, date=transfer_cali_date)
+    else:
+        smart_cor = smart.correct_smart_dark_current(flight, file, option=3, path=inpath)
+    outfile = f"{outdir}/{file.replace('.dat', '_cor.dat')}"
+    smart_cor.to_csv(outfile, sep="\t", float_format="%.0f")
+    log.info(f"Saved {outfile}")
+
+
 if __name__ == "__main__":
     import pylim.helpers as h
     from pylim import cirrus_hl, smart
@@ -29,35 +57,11 @@ if __name__ == "__main__":
     files = tqdm([file for file in os.listdir(inpath) if os.path.isfile(os.path.join(inpath, file))])
     files_debug = [file for file in os.listdir(inpath) if os.path.isfile(os.path.join(inpath, file))]
 
-
-    def make_dark_cur_cor_file(flight: str, file: str, inpath: str, transfer_cali_date: str, outdir: str) -> None:
-        """
-        Function to write a dark current corrected file to the given input file
-        Args:
-            flight: to which flight does the file belong to? (e.g. Flight_20210707a)
-            file: Standard SMART file name
-            inpath: input directory
-            transfer_cali_date: date of transfer calibration to use for the dark current measurements ("" or yyyymmdd)
-            outdir: output directory
-
-        Returns: Writes dark current corrected file to data_path in config.toml
-
-        """
-        log.info(f"Working on {file}")
-        if len(transfer_cali_date) > 0:
-            smart_cor = smart.correct_smart_dark_current(flight, file, option=2, path=inpath, date=transfer_cali_date)
-        else:
-            smart_cor = smart.correct_smart_dark_current(flight, file, option=2, path=inpath)
-        outfile = f"{outdir}/{file.replace('.dat', '_cor.dat')}"
-        smart_cor.to_csv(outfile, sep="\t", float_format="%.0f")
-        log.info(f"Saved {outfile}")
-
-
     # test one file
-    # file = files_debug[0]
-    # make_dark_cur_cor_file(file, inpath, transfer_cali_date, outdir)
+    # file = files_debug[1]
+    # make_dark_cur_cor_file(flight, file, inpath, transfer_cali_date, outdir)
     # for file in files_debug:
-    #     make_dark_cur_cor_file(file, inpath, transfer_cali_date, outdir)
+    #     make_dark_cur_cor_file(flight, file, inpath, transfer_cali_date, outdir)
     # run job in parallel
     Parallel(n_jobs=cpu_count()-2)(delayed(make_dark_cur_cor_file)(flight, file, inpath, transfer_cali_date, outdir)
                                    for file in files)
