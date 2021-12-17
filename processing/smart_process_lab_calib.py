1d3cb5836f5f6230ea9dc87b36650dd7adbaa3b0
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-12-17 14:38:28 +0100

    Update lab calibration processing
    - use folder date to decide on dark current file to use for VNIR measurement
    - use option 3 to correct VNIR measurements for dark current
    - more logging
    - remove redundant code
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/smart_process_lab_calib.py
+++ processing/smart_process_lab_calib.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 """Correct the lab calibration files for the dark current and merge the minutely files
-author: Johannes Röttenbacher
+
+**author**: Johannes Röttenbacher
 """
 if __name__ == "__main__":
-    # TODO: make script work for first lab calib
     import pylim.helpers as h
     from pylim import reader, smart
     import os
     import pandas as pd
     import logging
 
-    log = logging.getLogger(__name__)
+    log = logging.getLogger("pylim")
     log.addHandler(logging.StreamHandler())
     log.setLevel(logging.DEBUG)
 
     # User input
-    folder = "ASP06_Calib_Lab_20210809"
+    folder = "ASP06_Calib_Lab_20210329"
     # folder = "ASP07_Calib_Lab_20210809"
+    date = folder[-8:]  # extract date from folder name
+    if date == "20210809":
+        dark_file_J34 = "2021_08_09_10_51.Fdw_VNIR.dat"
+        dark_file_J56 = "2021_08_09_10_43.Fup_VNIR.dat"
+    elif date == "20210329":
+        dark_file_J34 = "2021_03_29_11_19.Fdw_VNIR.dat"
+        dark_file_J56 = "2021_03_29_11_09.Fup_VNIR.dat"
 
     # Set paths in config.toml
     calib_path = h.get_path("calib")
 
-    # merge VNIR dark measurement files before correcting the calib files
-    property = ["Iup", "Fup", "Fdw"]
+    log.info("Merging VNIR dark measurement files before correcting the calib files")
+    properties = ["Iup", "Fup", "Fdw"]
     for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
         log.info(f"Working on {dirpath}")
         if "dark" in dirpath:
-            for prop in property:
+            for prop in properties:
                 try:
                     vnir_dark_files = [f for f in files if f.endswith(f"{prop}_VNIR.dat")]
                     df = pd.concat([pd.read_csv(f"{dirpath}/{file}", sep="\t", header=None) for file in vnir_dark_files])
@@ -37,55 +44,45 @@ if __name__ == "__main__":
                     outname = f"{dirpath}/{vnir_dark_files[0]}"
                     df.to_csv(outname, sep="\t", index=False, header=False)
                     log.info(f"Saved {outname}")
-                except ValueError:
+                except ValueError as e:
+                    log.info(f"'{e}' encountered in. -> Moving on")
                     pass
 
-    dark_file_J34 = "2021_08_09_10_51.Fdw_VNIR.dat"
-    dark_file_J56 = "2021_08_09_10_43.Fup_VNIR.dat"
-
-    # correct all calibration measurement files for the dark current
+    log.info("Correcting all calibration measurement files for the dark current")
     for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
         log.info(f"Working on {dirpath}")
         for file in files:
             if file.endswith("SWIR.dat"):
                 log.info(f"Working on {dirpath}/{file}")
-                smart_cor = smart.correct_smart_dark_current("", file, option=2, path=dirpath)
-                outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
-                smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
-                log.info(f"Saved {outname}")
+                smart_cor = smart.correct_smart_dark_current("", file, option=3, path=dirpath)
 
             if file.endswith("VNIR.dat"):
                 log.info(f"Working on {dirpath}/{file}")
                 if "ASP06" in dirpath:
                     if "J3_4" in dirpath:
-                        dark_dir = f"{calib_path}/{folder}/Ulli_trans_J3_4_dark"
-                        dark_file = dark_file_J34
+                        dark_filepath = f"{calib_path}/{folder}/Ulli_trans_J3_4_dark/{dark_file_J34}"
                     elif "J5_6" in dirpath:
-                        dark_dir = f"{calib_path}/{folder}/Ulli_trans_J5_6_dark"
-                        dark_file = dark_file_J56
+                        dark_filepath = f"{calib_path}/{folder}/Ulli_trans_J5_6_dark/{dark_file_J56}"
                     else:
-                        print("Wrong dirpath!")
+                        log.info("Wrong dirpath!")
                         continue
 
                 else:
-                    dark_dir = f"{calib_path}/{folder}/dark_200ms"
-                    dark_file = "2021_08_09_06_57.Iup_VNIR.dat"
+                    dark_filepath = f"{calib_path}/{folder}/dark_200ms/2021_08_09_06_57.Iup_VNIR.dat"
 
-                measurement = reader.read_smart_raw(dirpath, file)
-                dark_current = reader.read_smart_raw(dark_dir, dark_file)
-                dark_current = dark_current.iloc[:, 2:].mean()
-                measurement = measurement.where(measurement.shutter == 1).iloc[:, 2:]  # only use data when shutter is open
-                smart_cor = measurement - dark_current
-                outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
-                smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
-                log.info(f"Saved {outname}")
+                smart_cor = smart.correct_smart_dark_current("", file, option=3, path=dirpath, dark_filepath=dark_filepath)
 
-    # merge minutely corrected files to one file
+            # write corrected smart measurement to new dat file
+            outname = f"{dirpath}/{file.replace('.dat', '_cor.dat')}"
+            smart_cor.to_csv(outname, sep="\t", float_format="%.0f")
+            log.info(f"Saved {outname}")
+
+    log.info("Merging minutely corrected files to one file")
     channels = ["SWIR", "VNIR"]
     for dirpath, dirs, files in os.walk(os.path.join(calib_path, folder)):
         log.info(f"Working on {dirpath}")
         for channel in channels:
-            for prop in property:
+            for prop in properties:
                 try:
                     filename = [file for file in files if file.endswith(f"{prop}_{channel}_cor.dat")]
                     df = pd.concat([pd.read_csv(f"{dirpath}/{file}", sep="\t", index_col="time") for file in filename])
