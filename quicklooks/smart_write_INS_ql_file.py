4dc4cf8cdb376ce5781ae3ebf14a62bf8bfba085
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-02-22 13:01:03 +0100

    Add GPS velocity data to SMART INS quicklook file
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- quicklooks/smart_write_INS_ql_file.py
+++ quicklooks/smart_write_INS_ql_file.py
@@ -9,20 +9,21 @@
 """
 
 if __name__ == "__main__":
-# %% module import
+    # %% module import
     import os
     import pandas as pd
+    import numpy as np
     import pylim.helpers as h
     from pylim import reader, solar_position
     from datetime import datetime
     from tqdm import tqdm
 
-# %% user input
+    # %% user input
     campaign = "halo-ac3"
     flight = "HALO-AC3_FD_00_HALO_Flight_00_20220221"
     date = flight[-8:]
 
-# %% set paths
+    # %% set paths
     base_path = f"E:/{campaign.swapcase()}_raw_only/02_Flights/{flight}"
     outpath = f"E:/{campaign.swapcase()}/02_Flights/{flight}/horidata"
     h.make_dir(outpath)
@@ -32,29 +33,36 @@ if __name__ == "__main__":
     nav_filepaths = [f"{hori_path}/{f}" for f in nav_files]
     gps_files = [f for f in hori_files if "Nav_GPSPos" in f]
     gps_filepaths = [f"{hori_path}/{f}" for f in gps_files]
+    vel_files = [f for f in hori_files if "Nav_GPSVel" in f]
+    vel_filepaths = [f"{hori_path}/{f}" for f in vel_files]
 
-# %% read in IMS and GPS data
+    # %% read in IMS and GPS data
     ims = pd.concat([reader.read_nav_data(f) for f in nav_filepaths])
     gps = pd.concat([reader.read_ins_gps_pos(f) for f in gps_filepaths])
+    vel = pd.concat([reader.read_ins_gps_vel(f) for f in vel_filepaths])
 
-# %% resample ims data to 1 Hz
+    # %% resample ims data to 1 Hz
     ims_1Hz = ims.resample("1s").asfreq()  # create a dataframe with a 1Hz index
     # reindex original dataframe and use the nearest values for the full seconds
     ims = ims.reindex_like(ims_1Hz, method="nearest")
 
-# %% merge dataframes
+    # %% merge dataframes
     df = ims.merge(gps, how="inner", on="time")
+    df = df.merge(vel, how="inner", on="time")
 
-# %% select only relevant columns
-    df = df.loc[:, ["seconds_y", "roll", "pitch", "yaw", "lat", "lon", "alt"]]
+    # %% select only relevant columns
+    df = df.loc[:, ["seconds_y", "roll", "pitch", "yaw", "lat", "lon", "alt", "v_east", "v_north", "v_up"]]
 
-# %% rename columns
+    # %% rename columns
     df = df.rename(columns=dict(seconds_y="seconds"))
 
-# %% IMS pitch is opposite to BAHAMAS pitch, switch signs so that they follow the same convention
+    # %% IMS pitch is opposite to BAHAMAS pitch, switch signs so that they follow the same convention
     df["pitch"] = -df["pitch"]
 
-# %% calculate solar zenith and azimuth angle
+    # %% calculate velocity from northerly and easterly part
+    df["vel"] = np.sqrt(df["v_east"]**2 + df["v_north"]**2)
+
+    # %% calculate solar zenith and azimuth angle
     dezimal_hours = df["seconds"] / 60 / 60
     year, month, day = df.index.year.values, df.index.month.values, df.index.day.values
     sza = list()
@@ -65,13 +73,13 @@ if __name__ == "__main__":
         # TODO: check SZA function why it returns sometimes an array sometimes a float
         saa.append(solar_position.get_saa(dezimal_hours[i], df["lat"][i], df["lon"][i], year[i], month[i], day[i]))
 
-# %% add to dataframe
+    # %% add to dataframe
     df["sza"], df["saa"] = sza, saa
 
-# %% convert to xarray
+    # %% convert to xarray
     ds = df.to_xarray()
 
-# %% create variable and global attributes
+    # %% create variable and global attributes
     var_attrs = dict(
         seconds=dict(
             long_name='Seconds since start of day',
@@ -108,7 +116,23 @@ if __name__ == "__main__":
         saa=dict(
             long_name='Solar azimuth angle',
             units='deg',
-            comment='South = 180, range: 0 to 360')
+            comment='South = 180, range: 0 to 360'),
+        v_east=dict(
+            long_name="Eastward velocity",
+            unit="m s^-1",
+            comment="Eastward velocity component as derived from the GPS sensor."),
+        v_north=dict(
+            long_name="Northward velocity",
+            unit="m s^-1",
+            comment="Northward velocity component as derived from the GPS sensor."),
+        v_up=dict(
+            long_name="Upward velocity",
+            unit="m s^-1",
+            comment="Vertical velocity as derived from the GPS sensor."),
+        vel=dict(
+            long_name="Ground speed",
+            unit="m s^-1",
+            comment="Ground speed calculated from northward and eastward velocity component: vel = sqrt(v_north^2 + v_east^2)")
     )
 
     global_attrs = dict(
@@ -136,25 +160,18 @@ if __name__ == "__main__":
 
     encoding = dict(
         time=dict(units=units, _FillValue=None),
-        seconds=dict(_FillValue=None),
-        roll=dict(_FillValue=None),
-        pitch=dict(_FillValue=None),
-        yaw=dict(_FillValue=None),
-        lat=dict(_FillValue=None),
-        lon=dict(_FillValue=None),
-        alt=dict(_FillValue=None),
-        sza=dict(_FillValue=None),
-        saa=dict(_FillValue=None)
     )
+    for var in var_attrs:
+        encoding[var] = dict(_FillValue=None)
     # TODO: simplify the setting of the _FillValue encoding -> loop -> function
 
-# %% assign meta data
+    # %% assign meta data
     for var in ds:
         ds[var].attrs = var_attrs[var]
 
     ds.attrs = global_attrs
 
-# %% create ncfile
+    # %% create ncfile
     outfile = f"{campaign.swapcase()}_HALO_SMART_IMS_ql_{date}.nc"
     out = os.path.join(outpath, outfile)
     ds.to_netcdf(out, format="NETCDF4_CLASSIC", encoding=encoding)
