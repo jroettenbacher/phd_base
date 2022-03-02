0152aa3917f54f0fe043245daa11983cb14e9db1
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-03-02 16:25:46 +0100

    Adapt to HALO-AC3
    - changed input to flight instead of date
    - renamed files to start with flight
    - changed year to 2022 in regular expression
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/gopro_write_timestamps.py
+++ processing/gopro_write_timestamps.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 """Read out timestamps from GoPro images and write to a text file (run on Ubuntu)
+
 * use exiftool to get timestamps of GoPro images
 * write the output to a textfile
 * read the textfile line by line and extract the picture number and datetime
 * convert to pd.datetime and correct for the time offset to BAHAMAS
 * create DataFrame and save to csv
-author: Johannes Röttenbacher
+
+*author*: Johannes Röttenbacher
 """
 if __name__ == "__main__":
     # %% module import
@@ -15,23 +17,24 @@ if __name__ == "__main__":
     import pandas as pd
 
     # %% set paths
-    date = 20210728
-    path = f"{h.get_path('gopro')}/{date}"
+    flight = "HALO-AC3_FD00_HALO_RF01_20220225"
+    date = flight[-8:]
+    path = f"{h.get_path('gopro')}/{flight}"
     # file = f"{path}/{date}_Gopro_0001.jpg"
 
     # write meta data info to file
-    with open(f"{path}/../{date}_timestamps.txt", "w+") as outfile:
+    with open(f"{path}/../{flight}_timestamps.txt", "w+") as outfile:
         run(['exiftool', '-m', '-DateTimeOriginal', path], stdout=outfile)
 
     # %% read the file and create a pandas data frame and write to csv again
     pic_num = list()
     timestamps = list()
-    with open(f"{path}/../{date}_timestamps.txt", "r") as ts:
+    with open(f"{path}/../{flight}_timestamps.txt", "r") as ts:
         for line in ts.readlines():
             if line.startswith("="):
-                pic_num.append(re.findall(r"2021\d{4}_Gopro_(?P<number>\d{4}).JPG", line)[0])
+                pic_num.append(re.findall(r"2022\d{4}_Gopro_(?P<number>\d{4}).JPG", line)[0])
             elif line.startswith("Date"):
-                timestamps.append(re.findall(r"2021:\d{2}:\d{2} \d{2}:\d{2}:\d{2}", line)[0])
+                timestamps.append(re.findall(r"2022:\d{2}:\d{2} \d{2}:\d{2}:\d{2}", line)[0])
             else:
                 pass
 
@@ -39,6 +42,6 @@ if __name__ == "__main__":
     ts_dt = pd.to_datetime(timestamps, format="%Y:%m:%d %H:%M:%S")
 
     # %% create a pandas data frame and write to csv
-    df = pd.DataFrame(dict(number=pic_num), index=ts_dt)\
-        .to_csv(f"{path}/../{date}_timestamps.csv", index_label="datetime")
-    print(f"Done with {date}")
+    df = pd.DataFrame(dict(number=pic_num), index=ts_dt).to_csv(f"{path}/../{flight}_timestamps.csv",
+                                                                index_label="datetime")
+    print(f"Done with {flight}")
