de0681c7ee91057b3f4a395f9b2fb18ba5f9c9c5
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-03-08 10:34:27 +0100

    Add Norderney radiosonde station
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- src/pylim/cirrus_hl.py
+++ src/pylim/cirrus_hl.py
@@ -53,11 +53,11 @@ stop_over_locations = dict(Flight_20210629a="Bergen", Flight_20210629b="Bergen",
 coordinates = dict(EDMO=(11.28, 48.08), Keflavik=(-22.6307, 63.976), Kiruna=(20.336, 67.821), Santiago=(-8.418, 42.898),
                    Bergen=(5.218, 60.293), Torshavn=(-6.76, 62.01), Muenchen_Oberschleissheim=(11.55, 48.25),
                    Meiningen=(10.38, 50.56), Lerwick=(-1.18, 60.13), Ittoqqortoormiit=(-21.95, 70.48),
-                   Tasiilaq=(-37.63, 65.60))
+                   Tasiilaq=(-37.63, 65.60), Norderney=(7.15, 53.71))
 
 # radiosonde stations
 radiosonde_stations = ["Torshavn_06011", "Muenchen_Oberschleissheim_10868", "Meiningen_10548", "Lerwick_03005",
-                       "Ittoqqortoormiit_04339", "Tasiilaq_04360"]
+                       "Ittoqqortoormiit_04339", "Tasiilaq_04360", "Norderney_10113"]
 
 # above, below and in cloud intervals for each flight
 flight_sections = dict(
