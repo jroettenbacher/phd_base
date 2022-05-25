60fefd649e7db69cc2e392498ee3283301875557
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-05-25 16:38:30 +0200

    Fix syntax error
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/libradtran_run_uvspec.py
+++ processing/libradtran_run_uvspec.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     except NameError:
         file = None
     log = h.setup_logging("./logs", file, flight_key)
-    log.INFO(f"Options Given:\ncampaign: {campaign}\nflight: {flight}\nwavelength: {wavelength}\n"
+    log.info(f"Options Given:\ncampaign: {campaign}\nflight: {flight}\nwavelength: {wavelength}\n"
              f"uvspec_exe: {uvspec_exe}\nScript started: {dt.datetime.utcnow():%c UTC}")
     # %% call uvspec for one file
     # index = 0
