e8c6f8ab4ab7abcc23be3bf451cf3125550d5272
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-09-08 15:46:53 +0200

    Add lists with info on stabilization
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- src/pylim/halo_ac3.py
+++ src/pylim/halo_ac3.py
@@ -68,3 +68,7 @@ take_offs_landings = dict(EMV=(Ts(2022, 2, 21, 10, 25), Ts(2022, 2, 21, 13, 42))
 # GoPro time offset to BAHAMAS
 gopro_offsets = dict(EMV=-2, RF00=24, RF01=118, RF02=123, RF03=127, RF04=0, RF05=21, RF06=-2, RF07=26, RF08=28, RF09=75,
                      RF10=79, RF11=85, RF12=97, RF13=5, RF14=24, RF15=28, RF16=41, RF17=46, RF18=64)
+
+stabilized_flights = list(flight_names.values())[:-2]  # flights during which the stabilization was running
+unstabilized_flights = list(flight_names.values())[-2:]  # flights with no stabilization running
+stabbi_offs = list(flight_names.values())[8:]  # flights during which the stabilization was turned off in curves
