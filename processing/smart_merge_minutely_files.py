7ac565fee51f0131d51d8c568fc9033020aaab53
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2022-09-08 09:10:31 +0200

    Add option to run script for all flights of one campaign
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/smart_merge_minutely_files.py
+++ processing/smart_merge_minutely_files.py
@@ -9,6 +9,7 @@ author: Johannes Röttenbacher
 """
 if __name__ == "__main__":
     import pylim.helpers as h
+    from pylim.cirrus_hl import transfer_calibs
     import os
     import pandas as pd
     import logging
@@ -18,38 +19,41 @@ if __name__ == "__main__":
     log.addHandler(logging.StreamHandler())
     log.setLevel(logging.INFO)
 
-    # combine minutely files to one file
     # User input
     campaign = "halo-ac3"
-    flight = "HALO-AC3_20220412_HALO_RF18"  # set flight folder
-    data_path = h.get_path("data", flight=flight, campaign=campaign)  # dark current corrected files
-    directory = data_path
+    # uncomment for single flight use
+    flights = ["HALO-AC3_20220312_HALO_RF02"]  # set flight folder
+    # uncomment for all flight loop
+    # flights = list(transfer_calibs.keys())[11:]  # list all flight keys
+    for flight in flights:
+        data_path = h.get_path("data", flight=flight, campaign=campaign)  # dark current corrected files
+        directory = data_path
 
-    channels = ["SWIR", "VNIR"]
-    props = ["Fdw"]
-    corrected = True  # merge dark current corrected files
-    cor = "_cor" if corrected else ""
-    for dirpath, dirs, files in os.walk(directory):
-        for prop in props:
-            for channel in channels:
-                try:
-                    filename = [file for file in files if file.endswith(f"{prop}_{channel}{cor}.dat")]
-                    if corrected:
-                        df = pd.concat(
-                            [pd.read_csv(f"{dirpath}/{file}", sep="\t", index_col="time") for file in
-                             tqdm(filename, desc=f"{prop}_{channel}")])
-                    else:
-                        df = pd.concat(
-                            [pd.read_csv(f"{dirpath}/{file}", sep="\t") for file in tqdm(filename,
-                                                                                         desc=f"{prop}_{channel}")])
-                    # delete all minutely files
-                    for file in filename:
-                        os.remove(os.path.join(dirpath, file))
-                    outname = f"{dirpath}/{filename[0]}"
-                    if corrected:
-                        df.to_csv(outname, sep="\t")
-                    else:
-                        df.to_csv(outname, sep="\t", index=False)
-                    log.info(f"Saved {outname}")
-                except ValueError:
-                    pass
+        channels = ["SWIR", "VNIR"]
+        props = ["Fdw", "Fup", "Iup"] if campaign == "cirrus-hl" else ["Fdw"]  # set available properties depending on campaign
+        corrected = True  # merge dark current corrected files
+        cor = "_cor" if corrected else ""
+        for dirpath, dirs, files in os.walk(directory):
+            for prop in props:
+                for channel in channels:
+                    try:
+                        filename = [file for file in files if file.endswith(f"{prop}_{channel}{cor}.dat")]
+                        if corrected:
+                            df = pd.concat(
+                                [pd.read_csv(f"{dirpath}/{file}", sep="\t", index_col="time") for file in
+                                 tqdm(filename, desc=f"{prop}_{channel}")])
+                        else:
+                            df = pd.concat(
+                                [pd.read_csv(f"{dirpath}/{file}", sep="\t") for file in tqdm(filename,
+                                                                                             desc=f"{prop}_{channel}")])
+                        # delete all minutely files
+                        for file in filename:
+                            os.remove(os.path.join(dirpath, file))
+                        outname = f"{dirpath}/{filename[0]}"
+                        if corrected:
+                            df.to_csv(outname, sep="\t")
+                        else:
+                            df.to_csv(outname, sep="\t", index=False)
+                        log.info(f"Saved {outname}")
+                    except ValueError:
+                        pass
