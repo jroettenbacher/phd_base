1883465c60a69d0f1efea4244df3c0744814642d
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-03-29 15:22:20 +0200

    Add more documentation and libradtran comparison
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- experiments/ecrad_experiment_v8.py
+++ experiments/ecrad_experiment_v8.py
@@ -12,7 +12,7 @@ Focus is on the above cloud section in the high north before the below cloud sec
 
 .. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_v8_iwc_along_track.png
 
-    Retrieved ice water content from VarCloud retrieval.
+    Retrieved ice water content from VarCloud retrieval interpolated to IFS full level pressure altitudes.
 
 Results
 ^^^^^^^^
@@ -32,17 +32,25 @@ The histograms show the same picture.
 
     Histogramm of difference between IWC IFS and IWC VarCloud.
 
-.. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_diff_re_ice_hist.png
+.. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_v1_v8_re_ice_psd.png
 
-    Histogramm of difference between |re-ice| IFS and |re-ice| VarCloud.
+    Histogramm of |re-ice| used in the IFS run and the VarCloud run.
 
-The differences in microphysical properties also translate to differences in optical properties of the cloud.
+The differences in microphysical properties also translate to differences in optical properties of the cloud such as higher total optical depth (integrated over all bands) in the IFS run.
+
+.. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_diff_od_int_along_track.png
+
+    Difference in total optical depth integrated over all shortwave bands between the IFS run and the VarCloud run.
+
+Finally, due to larger |re-ice| the ice cloud from the retrieval is less absorbing/reflecting than the IFS forecasted ice cloud.
 
 .. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_diff_flux_dn_sw_along_track.png
 
     Difference in broadband solar downward irradiance between IFS and VarCloud.
 
-Due to a larger |re-ice| the ice cloud from the retrieval is less absorbing/reflecting than the IFS forecasted ice cloud.
+.. figure:: figures/experiment_v8/HALO-AC3_20220411_HALO_RF17_ecrad_diff_flux_dn_sw_hist.png
+
+    Histogramm of difference in broadband solar downward irradiance between IFS and VarCloud.
 
 .. |re-ice| replace:: :math:`r_{eff, ice}`
 """
@@ -82,10 +90,12 @@ if __name__ == "__main__":
     ecrad_path = f"{h.get_path('ecrad', campaign=campaign)}/{date}/"
     ifs_path = f"{h.get_path('ifs', campaign=campaign)}/{date}"
     varcloud_path = h.get_path("varcloud", flight, campaign)
+    libradtran_path = h.get_path("libradtran_exp", flight, campaign)
     ecrad_input = "ecrad_merged_input_20220411_v2.nc"
     ecrad_v8 = "ecrad_merged_output_20220411_v8.nc"
     ecrad_v1 = "ecrad_merged_inout_20220411_v1.nc"
     varcloud_file = "VAR2LAGR_L1D_V1_AC3_HALO_RF17_A20220411_090100_152000_TS1_AS100_P20230306190254.nc"
+    libradtran_file = "HALO-AC3_HALO_libRadtran_simulation_varcloud_1min_20220411_RF17.nc"
 
     # %% get flight segments for case study period
     segmentation = ac3airborne.get_flight_segments()["HALO-AC3"]["HALO"][f"HALO-AC3_HALO_{key}"]
@@ -104,6 +114,9 @@ if __name__ == "__main__":
     varcloud = xr.open_dataset(f"{varcloud_path}/{varcloud_file}").swap_dims(time="Time", height="Height").rename(
         Time="time")
     varcloud = varcloud.rename(Varcloud_Cloud_Ice_Water_Content="iwc")
+    libradtran_ds = xr.open_dataset(f"{libradtran_path}/{libradtran_file}")  # read in libradtran data
+    libradtran_ds["altitude"] = libradtran_ds.altitude / 1000
+    libradtran_ds = libradtran_ds.rename(CIWD="iwc")
 
     # %% read in ecrad data
     ecrad_ds_input = xr.open_dataset(f"{ecrad_path}/{ecrad_input}")
@@ -123,10 +136,12 @@ if __name__ == "__main__":
     ecrad_ds_v1 = ecrad_ds_v1.sel(time=ecrad_ds_v8.time)
 
     # %% compute cloud ice water path
-    factor = ecrad_ds_v1.pressure_hl.diff(dim="half_level").to_numpy() / (9.80665 * ecrad_ds_v1.cloud_fraction.to_numpy())
+    factor = ecrad_ds_v1.pressure_hl.diff(dim="half_level").to_numpy() / (
+                9.80665 * ecrad_ds_v1.cloud_fraction.to_numpy())
     ecrad_ds_v1["iwp"] = (["time", "level"], factor * ecrad_ds_v1.ciwc.to_numpy())
     ecrad_ds_v1["iwp"] = ecrad_ds_v1.iwp.where(ecrad_ds_v1.iwp != np.inf, np.nan)
-    factor = ecrad_ds_v8.pressure_hl.diff(dim="half_level").to_numpy() / (9.80665 * ecrad_ds_v8.cloud_fraction.to_numpy())
+    factor = ecrad_ds_v8.pressure_hl.diff(dim="half_level").to_numpy() / (
+                9.80665 * ecrad_ds_v8.cloud_fraction.to_numpy())
     ecrad_ds_v8["iwp"] = (["time", "level"], factor * ecrad_ds_v8.ciwc.to_numpy())
     ecrad_ds_v8["iwp"] = ecrad_ds_v8.iwp.where(ecrad_ds_v8.iwp != np.inf, np.nan)
 
@@ -144,11 +159,13 @@ if __name__ == "__main__":
     ecrad_ds_v1["g"] = ice_optics_fu[2]
     ecrad_ds_v1["band_sw"] = range(1, 15)
     ecrad_ds_v1["band_lw"] = range(1, 17)
+    ecrad_ds_v1["absorption"] = ecrad_ds_v1["od"] - ecrad_ds_v1["scat_od"]
     ecrad_ds_v1["od_mean"] = ecrad_ds_v1["od"].mean(dim="band_sw")
     ecrad_ds_v1["scat_od_mean"] = ecrad_ds_v1["scat_od"].mean(dim="band_sw")
     ecrad_ds_v1["g_mean"] = ecrad_ds_v1["g"].mean(dim="band_sw")
     ecrad_ds_v1["scat_od_int"] = ecrad_ds_v1["scat_od"].integrate(coord="band_sw")
     ecrad_ds_v1["od_int"] = ecrad_ds_v1["od"].integrate(coord="band_sw")
+    ecrad_ds_v1["absorption_int"] = ecrad_ds_v1["absorption"].integrate(coord="band_sw")
     # version8
     ice_optics_fu = ecrad.calc_ice_optics_fu_sw(ecrad_ds_v8["iwp"], ecrad_ds_v8.re_ice)
     ecrad_ds_v8["od"] = ice_optics_fu[0]
@@ -156,19 +173,24 @@ if __name__ == "__main__":
     ecrad_ds_v8["g"] = ice_optics_fu[2]
     ecrad_ds_v8["band_sw"] = range(1, 15)
     ecrad_ds_v8["band_lw"] = range(1, 17)
+    ecrad_ds_v8["absorption"] = ecrad_ds_v8["od"] - ecrad_ds_v8["scat_od"]
     ecrad_ds_v8["od_mean"] = ecrad_ds_v8["od"].mean(dim="band_sw")
     ecrad_ds_v8["scat_od_mean"] = ecrad_ds_v8["scat_od"].mean(dim="band_sw")
     ecrad_ds_v8["g_mean"] = ecrad_ds_v8["g"].mean(dim="band_sw")
     ecrad_ds_v8["scat_od_int"] = ecrad_ds_v8["scat_od"].integrate(coord="band_sw")
     ecrad_ds_v8["od_int"] = ecrad_ds_v8["od"].integrate(coord="band_sw")
+    ecrad_ds_v8["absorption_int"] = ecrad_ds_v8["absorption"].integrate(coord="band_sw")
 
     # %% calculate other optical parameters
     ecrad_ds_v1["reflectivity_sw"] = ecrad_ds_v1.flux_up_sw / ecrad_ds_v1.flux_dn_sw
     ecrad_ds_v8["reflectivity_sw"] = ecrad_ds_v8.flux_up_sw / ecrad_ds_v8.flux_dn_sw
+    libradtran_ds["eglo_int"] = libradtran_ds["eglo"].integrate(coord="wavelength")
+    libradtran_ds["eup_int"] = libradtran_ds["eup"].integrate(coord="wavelength")
+    ecrad_ds_v8["flux_dn_sw_int"] = ecrad_ds_v8["spectral_flux_dn_sw"].sel(band_sw=slice(3, 11)).integrate(coord="band_sw")
 
     # %% set plotting options
-    var = "ciwc"
-    v = "v1"
+    var = "flux_dn_sw"
+    v = "diff"
     band = None
     band_str = f"_band{band}" if band is not None else ""
 
@@ -213,6 +235,7 @@ if __name__ == "__main__":
         # calculate difference between simulations
         ecrad_ds_diff = ecrad_ds_v1[var] - ecrad_ds_v8[var]
         ecrad_plot = ecrad_ds_diff.where((ecrad_ds_v8[var] != 0) | (~np.isnan(ecrad_ds_v8[var]))) * sf
+
     # add new z axis mean pressure altitude
     if "half_level" in ecrad_plot.dims:
         new_z = ecrad_ds_v8["press_height_hl"].mean(dim="time") / 1000
@@ -228,7 +251,8 @@ if __name__ == "__main__":
             tmp_plot = tmp_plot.rename(half_level="height")
 
         else:
-            tmp_plot = tmp_plot.assign_coords(level=ecrad_ds_v8["press_height_full"].sel(time=t, drop=True).to_numpy() / 1000)
+            tmp_plot = tmp_plot.assign_coords(
+                level=ecrad_ds_v8["press_height_full"].sel(time=t, drop=True).to_numpy() / 1000)
             tmp_plot = tmp_plot.rename(level="height")
 
         tmp_plot = tmp_plot.interp(height=new_z.to_numpy())
@@ -268,19 +292,20 @@ if __name__ == "__main__":
     plt.tight_layout()
     figname = f"{plot_path}/{flight}_ecrad_{v}_{var}{band_str}_along_track.png"
     plt.savefig(figname, dpi=300, bbox_inches="tight")
-    figname = f"{fig_path}/{flight}_ecrad_{v}_{var}{band_str}_along_track.png"
-    plt.savefig(figname, dpi=300, bbox_inches="tight")
+    # figname = f"{fig_path}/{flight}_ecrad_{v}_{var}{band_str}_along_track.png"
+    # plt.savefig(figname, dpi=300, bbox_inches="tight")
     plt.show()
     plt.close()
 
     # %% plot histogram
     xlabel = "Difference v1 - v8" if v == "diff" else v
     flat_array = ecrad_plot.to_numpy().flatten()
-    _, ax = plt.subplots(figsize=h.figsize_equal)
+    _, ax = plt.subplots(figsize=h.figsize_wide)
     ax.hist(flat_array, bins=20)
     ax.set(xlabel=f"{h.cbarlabels[var]} {xlabel} ({h.plot_units[var]})",
            ylabel="Number of Occurrence")
     ax.grid()
+    ax.set_yscale("log")
     plt.tight_layout()
     figname = f"{plot_path}/{flight}_ecrad_{v}_{var}{band_str}_hist.png"
     plt.savefig(figname, dpi=300, bbox_inches="tight")
@@ -312,6 +337,8 @@ if __name__ == "__main__":
            title=f"Particle Size Distribution between 10:30 and 12:30 UTC")
     figname = f"{plot_path}/{flight}_ecrad_v1_v8_re_ice_psd.png"
     plt.savefig(figname, dpi=300, bbox_inches="tight")
+    figname = f"{fig_path}/{flight}_ecrad_v1_v8_re_ice_psd.png"
+    plt.savefig(figname, dpi=300, bbox_inches="tight")
     plt.show()
     plt.close()
 
@@ -320,5 +347,39 @@ if __name__ == "__main__":
     factor = (v1.iwc / v8.iwc).where(~np.isnan(v8.iwc) & (v8.iwc != 0)).sel(level=slice(75, 138))
     # factor = factor / np.nanmax(factor)
     factor.plot(x="time", robust=True)
+    plt.show()
+    plt.close()
+
+    # %% plot libradtran simulation
+    var = "iwc"
+    cmap = h.cmaps[var] if var in h.cmaps else cmr.rainforest
+    plot_ds = libradtran_ds[var] * 1e3
+    plot_ds = plot_ds.where(plot_ds != 0)
+    te = pd.to_timedelta((plot_ds.time[-1] - plot_ds.time[0]).to_numpy())
+
+    _, ax = plt.subplots(figsize=h.figsize_wide)
+    plot_ds.plot(x="time", cmap=cmap, ax=ax,
+                 cbar_kwargs={"pad": 0.04, "label": f"{h.cbarlabels[var]} ({h.plot_units[var]})"})
+    ax.set(xlabel="Time (UTC)", ylabel="Altitude (km)", title="libRadtran varcloud simulation")
+    h.set_xticks_and_xlabels(ax, te)
+    plt.tight_layout()
+
+    figname = f"{plot_path}/{flight}_libradtran_{var}_along_track.png"
+    plt.savefig(figname, dpi=300, bbox_inches="tight")
+    plt.show()
+    plt.close()
+
+    # %% interpolate libradtran to ecrad_plot height axis
+    libradtran_inp = libradtran_ds.interp(altitude=np.flip(ecrad_plot.height))
+
+    # %% calcualte difference between ecrad and libradtran
+    diff_plot = ecrad_plot - libradtran_inp["eglo_int"]
+    _, ax = plt.subplots(figsize=h.figsize_wide)
+    diff_plot.plot(x="time", ax=ax,
+                   cbar_kwargs={"label": "Solar Downward Irradiance (W$\,$m$^{-2}$)"})
+    ax.set(xlabel="Time (UTC)", ylabel="Altitude (km)",
+           title="Difference between ecRad varcloud and libRadtran varcloud")
+    h.set_xticks_and_xlabels(ax, te)
+    plt.tight_layout()
     plt.show()
     plt.close()
