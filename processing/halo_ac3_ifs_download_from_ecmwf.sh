8d247ab2dfb0f97f06bda5b4c984454ff6a87327
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-06-22 14:44:44 +0200

    Adapt script to use for loops
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/halo_ac3_ifs_download_from_ecmwf.sh
+++ processing/halo_ac3_ifs_download_from_ecmwf.sh
@@ -4,223 +4,149 @@ cd /ec/res4/scratch/gdmw/scratch_jr || exit
 
 
 # download surface files for the complete area and multi level files
-latlon_area='90/-60/67/30'
+latlon_area='90/-60/67/30'  # HALO-(AC)3 area
+declare -a dates=(20220312 20220313 20220314 20220315 20220316 20220320 20220321 20220328 20220329 20220330 20220404 20220407 20220408 20220410 20220411 20220412)
+#declare -a dates=(20220411)  # if you only want to run one date
+declare -a times=(00 12)
+grid='F1280'  # regular (lat lon) gaussian grid
+grid='O1280'  # octahedral reduced gaussian grid (original grid)
 
 # loop over dates if needed
-for date in 20220312 20220313 20220314 20220315 20220316 20220320 20220321 20220328 20220329 20220330 20220404 20220407 20220408 20220410 20220411 20220412;
+for date in "${dates[@]}"
 do
     date_12=$(date +%Y%m%d -d "${date} - 1 day")  # define date before
-    mkdir -p ${date}
-    cd ${date} || exit
+    mkdir -p "${date}"
+    cd "${date}" || exit
 
-# write MARS surface retrievals for 12Z, 00Z and 12Z the day before
-# 12Z
-    cat > mars_ifs_surface_${date}_12 << EOF
+    for time in "${times[@]}"
+    do
+        # set end_step according to time
+        if [[ ${time} == 12 ]]; then
+            end_step=12  # select only 12 steps when starting at 12Z
+        else
+            end_step=24  # select 24 steps when starting at 00Z
+        fi
+        # write MARS surface retrievals for 00Z and 12Z
+        cat > mars_ifs_surface_"${date}"_"${time}" << EOF
 retrieve,
-        padding  = 0,
-        accuracy = 16,
-        class    = od,
-        expver   = 1,
-        stream   = oper,
-        domain   = g,
-        type     = fc,
-        date     = ${date},
-        time     = 12,
-        step     = 0/to/12/by/1,
-        target   = ifs_${date}_12_sfc.grb,
-        param    = 31/32/34/134/136/137/151/164/165/166/172/235/243/cbh/169/175/176/177,
-        repres   = sh,
-        area     = ${latlon_area},
-        resol    = 1279,
-        grid     = 1280,
-        gaussian = regular,
-        levtype  = sfc
+target   =ifs_${date}_${time}_sfc_${grid}.grb,
+levtype  =sfc,
+date     =${date},
+time     =${time},
+step     =0/to/${end_step}/by/1,
+grid     =${grid},
+accuracy =av,
+area     =${latlon_area},
+class    =od,
+padding  =0,
+param    =31/32/34/134/136/137/151/164/165/166/172/235/243/cbh/169/175/176/177,
+stream   =oper,
+type     =fc
 EOF
 
-# write bash script to call with sbatch which will then execute the mars request
-# due to a new version of slurm calling mars with sbatch is no longer possible...
-
-   cat > mars_ifs_surface_${date}_12.sh << EOF
+        # write bash script to call with sbatch which will then execute the mars request
+        # due to a new version of slurm calling mars with sbatch is no longer possible...
+       cat > mars_ifs_surface_"${date}"_"${time}".sh << EOF
 #!/bin/bash
 #SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
 cd /ec/res4/scratch/gdmw/scratch_jr/${date}
-mars mars_ifs_surface_${date}_12
+mars mars_ifs_surface_${date}_${time}
 EOF
+        # start sbatch job for surface
+        sbatch --job-name=mars_ifs_surface_"${date}"_"${time}" --time=05:00:00 --dependency=singleton mars_ifs_surface_"${date}"_"${time}".sh
 
-   sbatch --job-name=mars_ifs_surface_012 --time=05:00:00 --dependency=singleton mars_ifs_surface_${date}_12.sh
-
-# 00Z
-    cat <<EOF > mars_ifs_surface_${date}_00
+        # write MARS retrievals for multi level files for 00Z and 12Z
+        cat > mars_ifs_ml_"${date}"_"${time}" <<EOF
 retrieve,
-        padding  = 0,
-        accuracy = 16,
-        class    = od,
-        expver   = 1,
-        stream   = oper,
-        domain   = g,
-        type     = fc,
-        date     = ${date},
-        time     = 00,
-        step     = 0/to/24/by/1,
-        target   = ifs_${date}_00_sfc.grb,
-        param    = 31/32/34/134/136/137/151/164/165/166/172/235/243/cbh/169/175/176/177,
-        repres   = sh,
-        area     = ${latlon_area},
-        resol    = 1279,
-        grid     = 1280,
-        gaussian = regular,
-        levtype  = sfc
+target   =ifs_${date}_${time}_ml_${grid}.grb,
+levtype  =ml,
+levellist=1/to/137,
+date     =${date},
+time     =${time},
+grid     =${grid},
+step     =0/to/${end_step}/by/1,
+param    =75/76/130/131/132/133/135/152/203/246/247/248,
+accuracy =av,
+area     =${latlon_area},
+class    =od,
+padding  =0,
+stream   =oper,
+type     =fc
 EOF
 
-# write bash script to call with sbatch which will then execute the mars request
-# due to a new version of slurm calling mars with sbatch is no longer possible...
-
-   cat > mars_ifs_surface_${date}_00.sh << EOF
+        # write bash script to call with sbatch which will then execute the mars request
+        # due to a new version of slurm calling mars with sbatch is no longer possible...
+        cat > mars_ifs_ml_"${date}"_"${time}".sh << EOF
 #!/bin/bash
 #SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
 cd /ec/res4/scratch/gdmw/scratch_jr/${date}
-mars mars_ifs_surface_${date}_00
+mars mars_ifs_ml_${date}_${time}
 EOF
+        # start sbatch job for multi levels
+        sbatch --job-name=mars_ifs_ml_"${date}"_"${time}" --time=05:00:00 --dependency=singleton mars_ifs_ml_"${date}"_"${time}".sh
 
-    sbatch --job-name=mars_ifs_surface_00 --time=05:00:00 --dependency=singleton mars_ifs_surface_${date}_00.sh
+    done
 
-# 12Z the day before
-    cat <<EOF > mars_ifs_surface_${date_12}_12
+    # write retrievals for 12Z the day before if the day before is not in the dates list
+    if [[ ! " ${dates[*]} " =~ ${date_12} ]]; then
+        # write surface retrieval
+        cat > mars_ifs_surface_"${date_12}"_12 <<EOF
 retrieve,
-        padding  = 0,
-        accuracy = 16,
-        class    = od,
-        expver   = 1,
-        stream   = oper,
-                domain   = g,
-        type     = fc,
-        date     = ${date_12},
-        time     = 12,
-        step     = 12/to/36/by/1,
-        target   = ifs_${date_12}_12_sfc.grb,
-        param    = 31/32/34/134/136/137/151/164/165/166/172/235/243/cbh/169/175/176/177,
-        repres   = sh,
-        area     = ${latlon_area},
-        resol    = 1279,
-        grid     = 1280,
-        gaussian = regular,
-        levtype  = sfc
+target   =ifs_${date_12}_12_sfc_${grid}.grb,
+levtype  =sfc,
+date     =${date},
+time     =12,
+step     =12/to/36/by/1,
+grid     =${grid},
+accuracy =av,
+area     =${latlon_area},
+class    =od,
+padding  =0,
+param    =31/32/34/134/136/137/151/164/165/166/172/235/243/cbh/169/175/176/177,
+stream   =oper,
+type     =fc
 EOF
 
-# write bash script to call with sbatch which will then execute the mars request
-# due to a new version of slurm calling mars with sbatch is no longer possible...
-
-   cat > mars_ifs_surface_${date_12}_12.sh << EOF
+        # write bash script to call with sbatch which will then execute the mars request
+        # due to a new version of slurm calling mars with sbatch is no longer possible...
+        cat > mars_ifs_surface_"${date_12}"_12.sh << EOF
 #!/bin/bash
 #SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
 cd /ec/res4/scratch/gdmw/scratch_jr/${date}
 mars mars_ifs_surface_${date_12}_12
 EOF
 
-    sbatch --job-name=mars_ifs_surface_12 --time=05:00:00 --dependency=singleton mars_ifs_surface_${date_12}_12.sh
+        sbatch --job-name=mars_ifs_surface_"${date_12}"_12 --time=05:00:00 --dependency=singleton mars_ifs_surface_"${date_12}"_12.sh
 
-# write MARS retrievals for multi level files for 12Z, 00Z and 12Z the day before
-# 12Z
-
-    cat <<EOF > mars_ifs_ml_${date}_12
+        # write multilevel retrieval
+        cat > mars_ifs_ml_"${date_12}"_12 <<EOF
 retrieve,
-        padding  = 0,
-        accuracy = 16,
-        class    = od,
-        expver   = 1,
-        stream   = oper,
-        domain   = g,
-        type     = fc,
-        date     = ${date},
-        time     = 12,
-        step     = 0/to/12/by/1,
-        target   = ifs_${date}_12_ml.grb,
-        param    = 75/76/130/131/132/133/135/152/203/246/247/248,
-        repres   = sh,
-        area     = ${latlon_area},
-        resol    = 1279,
-        grid     = 1280,
-        gaussian = regular,
-        levtype  = ml,
-        levellist = 1/to/137
+target   =ifs_${date_12}_12_ml_${grid}.grb,
+levtype  =ml,
+levellist=1/to/137,
+date     =${date},
+time     =12,
+step     =12/to/36/by/1,
+grid     =${grid},
+param    =54/75/76/130/131/132/133/135/152/246/247/248,
+accuracy =av,
+area     =${latlon_area},
+class    =od,
+padding  =0,
+stream   =oper,
+type     =fc
 EOF
 
-# write bash script to call with sbatch which will then execute the mars request
-# due to a new version of slurm calling mars with sbatch is no longer possible...
-
-   cat > mars_ifs_ml_${date}_12.sh << EOF
-#!/bin/bash
-#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
-cd /ec/res4/scratch/gdmw/scratch_jr/${date}
-mars mars_ifs_ml_${date}_12
-EOF
-
-    sbatch --job-name=mars_ifs_ml_012 --time=05:00:00 --dependency=singleton mars_ifs_ml_${date}_12.sh
-
-# 00Z
-    cat <<EOF > mars_ifs_ml_${date}_00
-retrieve,
-        padding  = 0,
-        accuracy = 16,
-        class    = od,
-        expver   = 1,
-        stream   = oper,
-        domain   = g,
-        type     = fc,
-        date     = ${date},
-        time     = 00     ,
-        step     = 0/to/24/by/1,
-        target   = ifs_${date}_00_ml.grb,
-        param    = 54/75/76/130/131/132/133/135/152/246/247/248,
-        repres   = sh,
-        area     = ${latlon_area},
-        resol    = 1279,
-        grid     = 1280,
-        gaussian = regular,
-        levtype  = ml,
-        levellist = 1/to/137
-EOF
-
-   cat > mars_ifs_ml_${date}_00.sh << EOF
-#!/bin/bash
-#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
-cd /ec/res4/scratch/gdmw/scratch_jr/${date}
-mars mars_ifs_ml_${date}_00
-EOF
-
-   sbatch --job-name=mars_ifs_ml_00 --time=05:00:00 --dependency=singleton mars_ifs_ml_${date}_00.sh
-
-# 12Z the day before
-    cat <<EOF > mars_ifs_ml_${date_12}_12
-retrieve,
-        accuracy = 16,
-        area     = ${latlon_area},
-        class    = od,
-        date     = ${date_12},
-        domain   = g,
-        expver   = 1,
-        grid     = 1280,
-        type     = fc,
-        levellist = 1/to/137
-        levtype  = ml,
-        padding  = 0,
-        param    = 54/75/76/130/131/132/133/135/152/246/247/248,
-        repres   = sh,
-        resol    = 1279,
-        stream   = oper,
-        step     = 12/to/36/by/1,
-        target   = ifs_${date_12}_12_ml.grb,
-        time     = 12,
-EOF
-
-   cat > mars_ifs_ml_${date_12}_12.sh << EOF
+        cat > mars_ifs_ml_"${date_12}"_12.sh << EOF
 #!/bin/bash
 #SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/${date}
 cd /ec/res4/scratch/gdmw/scratch_jr/${date}
 mars mars_ifs_ml_${date_12}_12
 EOF
 
-    sbatch --job-name=mars_ifs_ml_12 --time=05:00:00 --dependency=singleton mars_ifs_ml_${date_12}_12.sh
+        sbatch --job-name=mars_ifs_ml_"${date}"_12 --time=05:00:00 --dependency=singleton mars_ifs_ml_"${date_12}"_12.sh
+
+    fi
 
 cd ..
 
