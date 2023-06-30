ccbb7b80e83e567f9e1ac9818bee7243160f9058
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-06-30 12:26:10 +0200

    Update paths
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/halo_ac3_cams_download_from_ecmwf.sh
+++ processing/halo_ac3_cams_download_from_ecmwf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr
+#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/cams_data
 set -f
-cd /ec/res4/scratch/gdmw/scratch_jr/cams || exit
+cd /ec/res4/scratch/gdmw/scratch_jr/cams_data || exit
 
 
 # download files for the complete area and multi level files
 latlon_area='90/-60/67/30'  # HALO-(AC)3 area
-declare -a years=($(seq 2003 1 2020))
+declare -a years=($(seq 2003 1 2022))
 declare -a months=(01 02 03 04 05 06 07 08 09 10 11 12)
 declare -a months=(03 04)  # HALO-(AC)3 months
 
@@ -39,8 +39,8 @@ EOF
     # due to a new version of slurm calling mars with sbatch is no longer possible...
         cat > mars_cams_ml_halo_ac3_"${m}".sh << EOF
 #!/bin/bash
-#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/cams/${year}
-cd /ec/res4/scratch/gdmw/scratch_jr/cams/${year}
+#SBATCH --chdir=/ec/res4/scratch/gdmw/scratch_jr/cams_data/${year}
+cd /ec/res4/scratch/gdmw/scratch_jr/cams_data/${year}
 mars mars_cams_ml_halo_ac3_${m}
 EOF
         # start sbatch job for multi levels
@@ -51,4 +51,3 @@ EOF
     cd ..
 
 done  # years
-
