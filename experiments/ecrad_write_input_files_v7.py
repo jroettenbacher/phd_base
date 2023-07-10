b7df990a82d20dbde8acb35173947e60d610d087
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-07-10 15:45:28 +0200

    Update ecrad_write_input_files_v7.py
    - remove unnecessary variables
    - rename fake_time to sim_time to represent its meaning better
    - reverse varcloud_ds in time
    - use Bahamas data to select closest IFS data
    - use a for loop instead of the Parallel function as this causes a PicklingError (don't know why)
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- experiments/ecrad_write_input_files_v7.py
+++ experiments/ecrad_write_input_files_v7.py
@@ -8,7 +8,8 @@ Use the VarCloud retrieval for the below cloud section.
 
 **Required User Input:**
 
-* step: at which intervals should the IFS data be interpolated on the aircraft data (default: 1Min from :ref:`processing:ecrad_read_ifs.py`)
+* key (flight key, eg. RF17)
+* init_time (init time of IFS data, eg. 00)
 
 **Output:**
 
@@ -20,10 +21,8 @@ if __name__ == "__main__":
     # %% module import
     import pylim.helpers as h
     import pylim.solar_position as sp
-    from pylim.ecrad import apply_ice_effective_radius, apply_liquid_effective_radius, calculate_pressure_height
+    from pylim.ecrad import apply_liquid_effective_radius
     from pylim import reader
-    import ac3airborne
-    from ac3airborne.tools import flightphase
     from metpy.calc import density
     from metpy.units import units
     import numpy as np
@@ -31,11 +30,9 @@ if __name__ == "__main__":
     import xarray as xr
     import os
     import pandas as pd
-    from datetime import datetime
     import time
-    from distutils.util import strtobool
     from tqdm import tqdm
-    from joblib import Parallel, delayed, cpu_count
+    # from joblib import cpu_count, delayed, Parallel
 
     start = time.time()
 
@@ -44,7 +41,6 @@ if __name__ == "__main__":
     version = "v7"
     args = h.read_command_line_args()
     key = args["key"] if "key" in args else "RF17"
-    # set interpolate flag
     init_time = args["init"] if "init" in args else "00"
     if campaign == "halo-ac3":
         import pylim.halo_ac3 as meta
@@ -56,7 +52,7 @@ if __name__ == "__main__":
 
         flight = key
         date = flight[7:15]
-    dt_day = datetime.strptime(date, '%Y%m%d')  # convert date to date time for further use
+
     # setup logging
     try:
         file = __file__
@@ -77,29 +73,6 @@ if __name__ == "__main__":
     # create output path
     h.make_dir(path_ecrad)
 
-    # %% get flight segments for case study period
-    segmentation = ac3airborne.get_flight_segments()["HALO-AC3"]["HALO"][f"HALO-AC3_HALO_{key}"]
-    segments = flightphase.FlightPhaseFile(segmentation)
-    above_cloud, below_cloud = dict(), dict()
-    if key == "RF17":
-        above_cloud["start"] = segments.select("name", "high level 7")[0]["start"]
-        above_cloud["end"] = segments.select("name", "high level 8")[0]["end"]
-        below_cloud["start"] = segments.select("name", "high level 9")[0]["start"]
-        below_cloud["end"] = segments.select("name", "high level 10")[0]["end"]
-        above_slice = slice(above_cloud["start"], above_cloud["end"])
-        below_slice = slice(below_cloud["start"], below_cloud["end"])
-        case_slice = slice(pd.to_datetime("2022-04-11 10:30"), pd.to_datetime("2022-04-11 12:29"))
-    else:
-        above_cloud["start"] = segments.select("name", "polygon pattern 1")[0]["start"]
-        above_cloud["end"] = segments.select("name", "polygon pattern 1")[0]["parts"][-1]["start"]
-        below_cloud["start"] = segments.select("name", "polygon pattern 2")[0]["start"]
-        below_cloud["end"] = segments.select("name", "polygon pattern 2")[0]["end"]
-        above_slice = slice(above_cloud["start"], above_cloud["end"])
-        below_slice = slice(below_cloud["start"], below_cloud["end"])
-        case_slice = slice(above_cloud["start"], below_cloud["end"])
-
-    time_extend_cs = below_cloud["end"] - above_cloud["start"]  # time extend for case study
-
     # %% read in intermediate files from read_ifs
     if init_time == "yesterday":
         ifs_date = int(date) - 1
@@ -115,24 +88,22 @@ if __name__ == "__main__":
 
     # %% select only case study time which features the cloud that HALO also underpassed
     sel_time = slice(pd.to_datetime("2022-04-11 10:49"), pd.to_datetime("2022-04-11 11:04"))
-    fake_time = pd.date_range("2022-04-11 11:35", "2022-04-11 11:50", freq="1s")
+    sim_time = pd.date_range("2022-04-11 11:35", "2022-04-11 11:50", freq="1s")
 
     # %% resample varcloud data to secondly resolution
     new_index = pd.date_range(str(varcloud_ds.time[0].astype('datetime64[s]').to_numpy()),
                               str(varcloud_ds.time[-1].astype('datetime64[s]').to_numpy()), freq="1s")
     varcloud_ds = varcloud_ds.reindex(time=new_index, method="bfill")
     varcloud_ds = varcloud_ds.sel(time=sel_time)
+    # reverse varcloud data as the last retrieval point is the first point that HALO underpasses on its way back
+    varcloud_ds = varcloud_ds.sortby("time", ascending=False)
 
-    # %% calculate pressure height for model levels if needed
-    if "press_height_full" in data_ml.data_vars:
-        log.debug("Pressure Height already in dataset. Moving on.")
-        pass
-    else:
-        ds_sel = calculate_pressure_height(data_ml)
+    # %% select bahamas data for simulation
+    bahamas_ds = bahamas_ds.sel(time=sim_time)
 
 # %% loop through time steps and write one file per time step
-    lats, lons, times = varcloud_ds.Latitude, varcloud_ds.Longitude, varcloud_ds.time
-    idx = len(times)
+    lats, lons = bahamas_ds.IRS_LAT.sel(time=sim_time), bahamas_ds.IRS_LON.sel(time=sim_time)
+    idx = len(varcloud_ds.time)
     ifs_lat_lon = np.column_stack((data_ml.lat, data_ml.lon))
     ifs_tree = ssp.KDTree(ifs_lat_lon)  # build the kd tree for nearest neighbour look up
     # generate an array with lat, lon values from the flight position
@@ -140,7 +111,8 @@ if __name__ == "__main__":
     dist, idxs = ifs_tree.query(points, k=1)  # query the tree
     closest_latlons = ifs_tree.data[idxs]
 
-    def write_ecrad_input_file(data_ml, closest_latlons, times, path_ecrad, i):
+
+    def write_ecrad_input_file(data_ml, varcloud_ds, bahamas_ds, closest_latlons, sim_time, path_ecrad, i):
         """
         Helper function to be called in parallel to speed up file creation.
         Variables are from outer script.
@@ -150,8 +122,9 @@ if __name__ == "__main__":
         """
         # select the nearest grid point to flight path
         latlon_sel = (closest_latlons[i][0], closest_latlons[i][1])
+        t = sim_time[i]
         ds_sel = data_ml.sel(rgrid=latlon_sel)
-        dsi_ml_out = ds_sel.sel(time=times[i], method="nearest").reset_coords("time")
+        dsi_ml_out = ds_sel.sel(time=t, method="nearest").reset_coords("time")
         varcloud_sel = varcloud_ds.isel(time=i)
         # interpolate varcloud height to model height
         varcloud_sel = varcloud_sel.interp(Height=dsi_ml_out.press_height_full).reset_coords(["time", "Height"])
@@ -161,15 +134,14 @@ if __name__ == "__main__":
         q_ice = varcloud_sel["Varcloud_Cloud_Ice_Water_Content"] * units("kg/m3") / air_density
         # overwrite ice water content
         dsi_ml_out["q_ice"] = q_ice.metpy.dequantify().where(~np.isnan(q_ice), 0)
-        t = fake_time[i]
 
-        # add cos_sza for the grid point using only model data
+        # add cos_sza for the grid point
         sod = t.hour * 3600 + t.minute * 60 + t.second
         p_surf_nearest = dsi_ml_out.pressure_hl.isel(half_level=137).to_numpy() / 100  # hPa
         t_surf_nearest = dsi_ml_out.temperature_hl.isel(half_level=137).to_numpy() - 273.15  # degree Celsius
         ypos = bahamas_ds.IRS_LAT.sel(time=t).to_numpy()
         xpos = bahamas_ds.IRS_LON.sel(time=t).to_numpy()
-        sza = sp.get_sza(sod / 3600, ypos, xpos, dt_day.year, dt_day.month, dt_day.day, p_surf_nearest, t_surf_nearest)
+        sza = sp.get_sza(sod / 3600, ypos, xpos, t.year, t.month, t.day, p_surf_nearest, t_surf_nearest)
         cos_sza = np.cos(sza / 180. * np.pi)
 
         dsi_ml_out["cos_solar_zenith_angle"] = xr.DataArray(cos_sza,
@@ -180,7 +152,7 @@ if __name__ == "__main__":
         re_ice = varcloud_sel["Varcloud_Cloud_Ice_Effective_Radius"]
         dsi_ml_out["re_ice"] = re_ice.where(~np.isnan(re_ice), 51.9616 * 1e-6)  # replace nan with default value
         dsi_ml_out = apply_liquid_effective_radius(dsi_ml_out)
-        #
+        # reset coordinates, drop unused dimension and unused variable
         dsi_ml_out = dsi_ml_out.reset_coords(["rgrid", "lat", "lon"]).drop_dims("reduced_points").drop_vars("rgrid")
         dsi_ml_out = dsi_ml_out.expand_dims("column", axis=0)
         # remove column dim from dimensionless variables
@@ -202,10 +174,12 @@ if __name__ == "__main__":
         return None
 
 
-    # This causes a PickleError... use a normal for loop instead
-    # Parallel(n_jobs=cpu_count() - 2)(delayed(write_ecrad_input_file)(data_ml, closest_latlons, times, path_ecrad, i)
+    # somehow this throws a PickleError
+    # Parallel(n_jobs=cpu_count() - 2)(delayed(write_ecrad_input_file)(data_ml, varcloud_ds, bahamas_ds, closest_latlons,
+    #                                                                  sim_time.to_numpy(), path_ecrad, i)
     #                                  for i in tqdm(range(0, idx)))
-    for i in tqdm(range(idx)):
-        write_ecrad_input_file(data_ml, closest_latlons, times, path_ecrad, i)
+
+    for i in tqdm(range(0, idx)):
+        write_ecrad_input_file(data_ml, varcloud_ds, bahamas_ds, closest_latlons, sim_time, path_ecrad, i)
 
     log.info(f"Done with date {date}: {pd.to_timedelta((time.time() - start), unit='second')} (hr:min:sec)")
