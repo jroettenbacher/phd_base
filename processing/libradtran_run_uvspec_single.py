a15148866caf112686c989bfbbade8c93557ac22
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-09-08 11:55:27 +0200

    Update file to use meta depending on campaign
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- processing/libradtran_run_uvspec_single.py
+++ processing/libradtran_run_uvspec_single.py
@@ -23,15 +23,20 @@ if __name__ == "__main__":
     import datetime as dt
 
 # %% set options and get files
-    campaign = "cirrus-hl"
-    flight = "Flight_20210628a"
-    input_file = ""
-    flight_key = flight[-4:] if campaign == "halo-ac3" else flight
+    campaign = "halo-ac3"
+    if campaign == "halo-ac3":
+        import pylim.halo_ac3 as meta
+    else:
+        import pylim.cirrus_hl as meta
+    key = "RF17"
+    flight = meta.flight_names[key] if campaign == "halo-ac3" else key
+    input_file = "20220411_104353_libRadtran.inp"
     date = flight[9:17] if campaign == "halo-ac3" else flight[7:15]
-    wavelength = "smart_spectral"  # will be used as directory name and in outfile name (e.g. smart, bacardi, 500-600nm, ...)
-    uvspec_exe = "/opt/libradtran/2.0.3/bin/uvspec"
+    wavelength = "test"  # will be used as directory name and in outfile name (e.g. smart, bacardi, 500-600nm, ...)
+    uvspec_exe = "/opt/libradtran/2.0.4/bin/uvspec"
     libradtran_base_dir = h.get_path("libradtran", flight, campaign)
     libradtran_dir = os.path.join(libradtran_base_dir, "wkdir", wavelength)  # file where to find input files
+    h.make_dir(libradtran_dir)
     input_file = os.path.join(libradtran_dir, input_file)
     output_file = input_file.replace(".inp", ".out")
     error_log = input_file.replace(".inp", ".log")
@@ -41,7 +46,7 @@ if __name__ == "__main__":
         file = __file__
     except NameError:
         file = None
-    log = h.setup_logging("./logs", file, flight_key)
+    log = h.setup_logging("./logs", file, key)
     log.info(f"Options Given:\ncampaign: {campaign}\nflight: {flight}\nfile: {input_file}\nwavelength: {wavelength}\n"
              f"uvspec_exe: {uvspec_exe}\nScript started: {dt.datetime.utcnow():%c UTC}")
 # %% call uvspec for one file
