9ae7d9c35fd510e2638b7cbe1dd2821fa0cb0855
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2023-09-08 11:34:08 +0200

    Move global and variable attributes to libradtran.py
    Data is now moved to the libradtran.py script to keep it seperated from the actual processing.
    
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- src/pylim/libradtran.py
+++ src/pylim/libradtran.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Functions to process and plot libRadTran simulation files
+"""Functions to process and plot libRadtran simulation files
 
 *author*: Johannes Röttenbacher
 """
@@ -16,7 +16,125 @@ import logging
 
 log = logging.getLogger(__name__)
 
-# %% functions
+
+def get_netcdf_global_attributes(campaign: str, flight: str, experiment: str):
+    """
+    Return a dictionary with globa attributes for a libRadtran simulation
+
+    Args:
+        campaign: which campaign (cirrus-hl, halo-ac3)
+        flight: which flight
+        experiment: which experiment has been run
+
+    Returns: dictionary with global attributes
+
+    """
+
+    global_attributes = {
+        "cirrus-hl": dict(
+            title=f"Simulated downward and upward irradiance along flight track for experiment {experiment}",
+            Conventions="CF-1.9",
+            camapign_id=f"{campaign.swapcase()}",
+            platform_id="HALO",
+            version_id="1",
+            comment=f"CIRRUS-HL Campaign, Oberpfaffenhofen, Germany, {flight}",
+            contact="PI: m.wendisch@uni-leipzig.de, Data: johannes.roettenbacher@uni-leipzig.de",
+            history=f"Created {datetime.datetime.utcnow():%c} UTC",
+            institution="Leipzig Institute for Meteorology, Leipzig University, Stephanstr.3, 04103 Leipzig, Germany",
+            source="libRadtran 2.0.4",
+            references="Emde et al. 2016, 10.5194/gmd-9-1647-2016"
+        ),
+        "halo-ac3": dict(
+            title=f"Simulated downward and upward irradiance along flight track for experiment {experiment}",
+            Conventions="CF-1.9",
+            campaign_id=f"{campaign.swapcase()}",
+            platform_id="HALO",
+            version_id="1",
+            institution="Leipzig Institute for Meteorology, Leipzig, Germany, Stephanstr.3, 04103 Leipzig, Germany",
+            history=f"created {datetime.datetime.utcnow():%c} UTC",
+            contact="Johannes Röttenbacher, johannes.roettenbacher@uni-leipzig.de",
+            PI="André Ehrlich, a.ehrlich@uni-leipzig.de",
+            source="libRadtran 2.0.4",
+            references="Emde et al. 2016, 10.5194/gmd-9-1647-2016",
+        )
+    }
+
+    return global_attributes[campaign]
+
+
+def get_netcdf_variable_attributes(solar_flag: bool, integrate_str: str, wavelength_str: str):
+    """
+    Return a dictionary with variable attributes for a libRadtran simulation
+
+    Args:
+        solar_flag: whether the simulation was a solar or a terrestrial simulation
+        integrate_str: whether the simulations was integrated at the end or not
+        wavelength_str: wavelength range of the simulation
+
+    Returns: dictionary with variable attributes
+
+    """
+    # set up metadata for general variables which are shared between solar and terrestrial simulations
+    general_variables = dict(
+        latitude=dict(units="degrees_north", long_name="latitude", standard_name="latitude"),
+        longitude=dict(units="degrees_east", long_name="longitude", standard_name="longitude"),
+        saa=dict(units="degree", long_name="solar azimuth angle", standard_name="soalr_azimuth_angle",
+                 comment="clockwise from north"),
+        sza=dict(units="degree", long_name="solar zenith angle", standard_name="solar_zenith_angle",
+                 comment="0 deg = zenith"),
+        CLWD=dict(units="g m^-3", long_name="cloud liquid water density",
+                  standard_name="mass_concentration_of_cloud_liquid_water_in_air"),
+        CIWD=dict(units="g m^-3", long_name="cloud ice water density",
+                  standard_name="mass_concentration_of_cloud_ice_water_in_air"),
+        p=dict(units="hPa", long_name="atmospheric pressure", standard_name="air_pressure"),
+        T=dict(units="K", long_name="air temperature", standard_name="air_temperature"),
+        wavelength=dict(units="nm", long_name="wavelength", standard_name="radiation_wavelength"),
+        re_ice=dict(units="mum", long_name="input ice effective radius"),
+        iwc=dict(units="g m^-3", long_name="input ice water content")
+    )
+
+    # set up metadata dictionaries for solar (shortwave) flux
+    solar_variables = dict(
+        albedo=dict(units="1", long_name="surface albedo", standard_name="surface_albedo"),
+        altitude=dict(units="m", long_name="height above mean sea level", standard_name="altitude"),
+        direct_fraction=dict(units="1", long_name="direct fraction of downward irradiance", comment=wavelength_str),
+        edir=dict(units="W m-2", long_name=f"{integrate_str}direct beam irradiance",
+                  standard_name="direct_downwelling_shortwave_flux_in_air",
+                  comment=wavelength_str),
+        edn=dict(units="W m-2", long_name=f"{integrate_str}diffuse downward irradiance",
+                 standard_name="diffuse_downwelling_shortwave_flux_in_air_assuming_clear_sky",
+                 comment=wavelength_str),
+        eup=dict(units="W m-2", long_name=f"{integrate_str}diffuse upward irradiance",
+                 standard_name="surface_upwelling_shortwave_flux_in_air_assuming_clear_sky",
+                 comment=wavelength_str),
+        eglo=dict(units="W m-2", long_name=f"{integrate_str}global solar downward irradiance",
+                  standard_name="solar_irradiance", comment=wavelength_str),
+        enet=dict(units="W m-2", long_name=f"{integrate_str}net irradiance", comment=wavelength_str),
+        heat=dict(units="K day-1", long_name="heating rate"),
+    )
+
+    # set up metadata dictionaries for terrestrial (longwave) flux
+    terrrestrial_variables = dict(
+        albedo=dict(units="1", long_name="surface albedo", standard_name="surface_albedo"),
+        altitude=dict(units="m", long_name="height above mean sea level", standard_name="altitude"),
+        direct_fraction=dict(units="1", long_name="direct fraction of downward irradiance", comment=wavelength_str),
+        edir=dict(units="W m-2", long_name=f"{integrate_str}direct beam irradiance",
+                  standard_name="direct_downwelling_longwave_flux_in_air",
+                  comment=wavelength_str),
+        edn=dict(units="W m-2", long_name=f"{integrate_str}downward irradiance",
+                 standard_name="downwelling_longwave_flux_in_air_assuming_clear_sky",
+                 comment=wavelength_str),
+        eup=dict(units="W m-2", long_name=f"{integrate_str}upward irradiance",
+                 standard_name="surface_upwelling_longwave_flux_in_air_assuming_clear_sky",
+                 comment=wavelength_str),
+    )
+
+    if solar_flag:
+        attributes_dict = {**general_variables, **solar_variables}
+    else:
+        attributes_dict = {**general_variables, **terrrestrial_variables}
+
+    return attributes_dict
 
 
 def find_closest_radiosonde_station(latitude: float, longitude: float):
