0072b023a0c3100cb625a15fc49a6123630a4897
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2024-04-17 17:42:56 +0200

    Add analysis of sensitivity study
    Signed-off-by: Johannes Röttenbacher <johannes.roettenbacher@web.de>

--- analysis/02_chapter_thesis_figures.py
+++ analysis/02_chapter_thesis_figures.py
@@ -15,6 +15,8 @@ from matplotlib import ticker
 from metpy.constants import Rd
 from metpy.units import units as u
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import xarray as xr
 
 cbc = h.get_cb_friendly_colors('petroff_6')
@@ -23,6 +25,11 @@ cbc = h.get_cb_friendly_colors('petroff_6')
 ecrad_path = 'E:/ecrad_sensitivity_studies'
 save_path = 'C:/Users/Johannes/Documents/Doktor/manuscripts/_thesis/data'
 plot_path = 'C:/Users/Johannes/Documents/Doktor/manuscripts/_thesis/figure'
+
+# %% read in data
+ecrad_dict = dict()
+for v in ['v1', 'v2', 'v3']:
+    ecrad_dict[v] = xr.open_dataset(f'{ecrad_path}/ecrad_merged_inout_{v}.nc')
 
 # %% plot minimum ice effective radius from Sun2001 parameterization
 latitudes = np.arange(0, 91)
@@ -115,3 +122,92 @@ figname = f'{plot_path}/02_re_ice_parameterization_T-IWC-Lat_log.pdf'
 plt.savefig(figname, dpi=300)
 plt.show()
 plt.close()
+
+# %% plot profiles of sensitivity study
+plt.rc('font', size=9)
+fig, axs = plt.subplots(1, 3, figsize=(15 * h.cm, 9 * h.cm), layout='constrained')
+
+re_ice = '1.5e-05'
+q_ices = ['0.0001', '1e-05', '5e-06']
+vs = ['v1', 'v2', 'v3']
+lats = ['30', '80']
+titles = ['Fu-IFS', 'Yi2013', 'Baran2016']
+ls = ['-', '--', ':']
+labels = ['(a)', '(b)', '(c)']
+for i, v in enumerate(vs):
+    ax = axs[i]
+    ax.grid()
+    ax.set(
+        xlim=(75, 250),
+        ylim=(0, 10),
+        title=titles[i]
+    )
+    ax.fill_between(np.array([0, 250]), 5, 8, alpha=0.3, color='grey')
+    ax.text(0.02, 0.95, labels[i], transform=ax.transAxes)
+    for ii, lat in enumerate(lats):
+        for iii, q_ice in enumerate(q_ices):
+            ecrad_plot = ecrad_dict[v].sel(q_ice_dim=q_ice,
+                                           latitude_dim=lat,
+                                           re_ice_dim=re_ice,
+                                           column=0)
+            ax.plot(ecrad_plot.flux_dn_sw.to_numpy(),
+                    ecrad_plot.altitude.to_numpy(),
+                    color=cbc[ii],
+                    ls=ls[iii],
+                    )
+
+# create nice legend
+axs[0].plot([], color=None, ls='', label='Latitude (°N)')
+axs[0].plot([], color=cbc[0], label='30')
+axs[0].plot([], color=cbc[1], label='80')
+axs[0].plot([], color=None, ls='', label='IWC (g/kg)')
+axs[0].plot([], color='k', ls=ls[0], label='0.1')
+axs[0].plot([], color='k', ls=ls[1], label='0.01')
+axs[0].plot([], color='k', ls=ls[2], label='0.005')
+fig.legend(loc='outside right upper')
+
+# label the cloud
+axs[0].annotate('Cirrus', xy=(120, 8), xytext=(100, 9),
+                arrowprops=dict(arrowstyle='->'),
+                )
+axs[0].set(ylabel='Altitude (km)')
+axs[1].set(xlabel=r'Solar downward irradiance (W$\,$m$^{-2}$)')
+
+figname = f'{plot_path}/02_ecrad_sensitivity_study_flux_dn_sw.pdf'
+plt.savefig(figname, dpi=300)
+plt.show()
+plt.close()
+
+# %% get some statistics
+stats = list()
+variables = ['transmissivity_sw_above_cloud', 'flux_dn_sw']
+for v in vs:
+    for lat in lats:
+        for q_ice in q_ices:
+            for label, half_level in zip(['above cloud', 'below cloud'], [41.5, 44.5]):
+                for var in variables:
+                    ds = ecrad_dict[v].sel(q_ice_dim=q_ice,
+                                           latitude_dim=lat,
+                                           re_ice_dim=re_ice,
+                                           column=0,
+                                           half_level=half_level)
+                    stats.append((v, lat, q_ice, label, var, ds[var].to_numpy()))
+
+df = pd.DataFrame(stats, columns=['version', 'latitude', 'q_ice', 'altitude', 'variable', 'value'])
+
+# %% calculate difference of above to below cloud solar downward irradiance
+pivot_df = (df[df.variable == 'flux_dn_sw']
+            .pivot_table(index=['version', 'latitude', 'q_ice', 'variable'],
+                         columns='altitude',
+                         values='value')
+            .reset_index())
+pivot_df['difference'] = pivot_df['above cloud'] - pivot_df['below cloud']
+
+# %% plot scatter plot of difference
+plot_df = df[(df.variable == 'transmissivity_sw_above_cloud') & (df.altitude == 'below cloud')]
+plot_df.loc[:, ['latitude', 'value', 'q_ice']] = plot_df[['latitude', 'value', 'q_ice']].apply(pd.to_numeric)
+fig, ax = plt.subplots(figsize=(10 * h.cm, 10 * h.cm), layout='constrained')
+sns.scatterplot(plot_df, x='q_ice', y='value', hue='latitude', style='version', ax=ax)
+plt.show()
+plt.close()
+
